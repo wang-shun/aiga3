83ff6ae142b5d030631d667ae4ab09a0685b7894
陶岳锋 <taoyf2012@gmail.com>
2017-02-14 16:12:31 +0800

    第一版可运行版本提交,可访问http://127.0.0.1:8080/aiga3/test 得到数据

--- aiga3/src/test/java/service/AigaP2pFunctionPointServiceTest.java
+++ aiga3/src/test/java/service/AigaP2pFunctionPointServiceTest.java
@@ -1,7 +1,9 @@
 package service;
 
 import org.junit.Test;
+import org.springframework.test.context.ContextConfiguration;
 
+@ContextConfiguration(locations = { "/spring/applicationContext.xml" })
 public class AigaP2pFunctionPointServiceTest {
 	
 	@Test

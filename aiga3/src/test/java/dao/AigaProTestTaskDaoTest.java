54f185213dc013ea3cb0afdf7d7024188d33cb1f
陶岳锋 <taoyf2012@gmail.com>
2017-02-16 17:19:58 +0800

    增加test 提交

--- aiga3/src/test/java/dao/AigaProTestTaskDaoTest.java
+++ aiga3/src/test/java/dao/AigaProTestTaskDaoTest.java
@@ -1,5 +1,32 @@
 package dao;
 
+import java.math.BigDecimal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.ai.aiga.dao.AigaP2pFunctionPointDao;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(locations = { "/spring/applicationContext.xml" })
 public class AigaProTestTaskDaoTest {
+	
+	@Autowired
+	private AigaP2pFunctionPointDao dao;
+	
+	public void test1(){
+		System.out.println(dao.count());
+	}
+	
+	@Test
+	public void test2(){
+		System.out.println(dao.exists(BigDecimal.valueOf(20002)));
+	}
+	
+	
 
 }

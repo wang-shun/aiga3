ed2c78ae9748ea96e4b027e913eae2dc2a08f7d1
陶岳锋 <taoyf2012@gmail.com>
2017-02-21 20:16:02 +0800

    菜单功能提交

--- aiga3/src/main/java/com/ai/aiga/view/controller/FunctionController.java
+++ aiga3/src/main/java/com/ai/aiga/view/controller/FunctionController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.ai.aiga.domain.AigaFunction;
 import com.ai.aiga.service.FunctionSv;
 import com.ai.aiga.service.RoleSv;
 import com.ai.aiga.view.json.FunctionRequest;
@@ -17,19 +18,40 @@ public class FunctionController {
 	@Autowired
 	private FunctionSv functionSv;
 	
-	@RequestMapping(path = "/sys/func/list" )
-	public @ResponseBody JsonBean listxxx(
-			@RequestParam(name="bcd", defaultValue="10") int abc){
+	@RequestMapping(path = "/sys/menu/list" )
+	public @ResponseBody JsonBean list(){
 		JsonBean bean = new JsonBean();
 		bean.setBean(functionSv.findFunctions());
 		return bean;
 	}
 	
-	public @ResponseBody JsonBean save(FunctionRequest funcRequest){
+	@RequestMapping(path = "/sys/menu/save")
+	public @ResponseBody JsonBean save(FunctionRequest request){
 		JsonBean bean = new JsonBean();
-		functionSv.save(funcRequest);
+		functionSv.save(request);
 		return bean;
 	}
+	
+	@RequestMapping(path = "/sys/menu/update")
+	public @ResponseBody JsonBean update(FunctionRequest request){
+		JsonBean bean = new JsonBean();
+		functionSv.update(request);
+		return bean;
+	}
+	
+	@RequestMapping(path = "/sys/menu/del")
+	public @ResponseBody JsonBean update(@RequestParam Long funcId){
+		functionSv.delete(funcId);
+		return JsonBean.success;
+	}
+	
+	@RequestMapping(path = "/sys/menu/get")
+	public @ResponseBody JsonBean get(@RequestParam Long funcId){
+		JsonBean bean = new JsonBean();
+		bean.setBean(functionSv.findOne(funcId));
+		return bean;
+	}
+	
 	
 
 

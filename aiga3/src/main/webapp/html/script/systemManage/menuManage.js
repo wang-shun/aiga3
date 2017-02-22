c0ad5f3dfc932f250cc89e025ce78079e8173083
Ying <binhdu@foxmail.com>
2017-02-22 20:57:21 +0800

    功能菜单模块添加

---
+++ aiga3/src/main/webapp/html/script/systemManage/menuManage.js
@@ -0,0 +1,185 @@
+define(function(require,exports,module){
+
+	
+	
+	// 初始化菜单列表
+	srvMap.add("getMenulist", "systemManage/menuManage/getMenulist.json", "/sys/menu/list");
+    // 获取菜单信息
+	srvMap.add("getMenuinfo", "systemManage/menuManage/getMenuinfo.json", "/sys/menu/get");	
+    //获取保存结果
+    srvMap.add("addMenu", "systemManage/menuManage/addMenu.json", "/sys/menu/save");
+    //获取修改结果
+    srvMap.add("updateMenu", "systemManage/menuManage/updateMenu.json", "/sys/menu/update");	
+    //删除结果
+    srvMap.add("deleMenu", "systemManage/menuManage/deleMenu.json", "/sys/menu/del");	
+
+	// 模板对象
+    var Tpl = {
+        getMenuinfo: require('tpl/MenuMng/getMenuinfo.tpl')
+    };
+
+    // 容器对象
+    var Mod = {
+        getMenuinfo: $('#Page_menuInfo')
+    };
+
+
+    //操作状态
+    var Operate_state = "update";
+    //当前菜单id
+    var currentMenu = null;
+    
+
+	var MenuMng = {
+		init: function(){
+			this._render();
+		},
+		_render: function() {
+
+
+		var setting = {
+			check: {
+				enable: true
+			},
+			data: {
+				simpleData: {
+					enable: true,
+					idKey: "funcId",
+                    pIdKey: "parentId"					
+				}
+			},
+			callback:{
+				 onClick: function(event, treeId, treeNode){
+				 	currentMenu = treeNode.funcId;
+				 	Rose.ajax.getJson(srvMap.get('getMenuinfo'), '', function(json, status) {
+						if(status) {
+							var template = Handlebars.compile(Tpl.getMenuinfo);
+							console.log(json.data)
+				    		Mod.getMenuinfo.html(template(json.data));
+				    		
+						}
+					});
+				 }
+			}			
+		};
+		Rose.ajax.getJson(srvMap.get('getMenulist'), '', function(json, status) {
+				if(status) {
+					console.log(json.data)
+            		$.fn.zTree.init($("#treeDemo"), setting, json.data);
+				}
+	  	});		
+
+		
+			
+			
+
+
+
+			
+			//添加
+			$("#menuAdd").bind('click',function(){
+				Operate_state = "new";
+				
+				$("#funcCode").val("");
+  				$("#name").val("");
+  				$("#funcImg").val(""),
+				$("#funcType option:selected").text("");
+  				$("#funcArg").val("");
+  				$("#dllPath").val("");
+  				$("#viewname").val("");
+  				$("#notes").val("");	
+	  		})
+
+	  		//保存
+	  		$("#menuSave").bind('click',function(){
+	  			if(Operate_state == "new"){
+		  			var cmd = {
+		  				"patentId":currentMenu,
+		  				"funcCode":$("#funcCode").val(),
+		  				"name":$("#name").val(),
+		  				"funcImg":$("#funcImg").val(),
+						"funcType":$("#funcType option:selected").text(),
+		  				"funcArg":$("#funcArg").val(),
+		  				"dllPath":$("#dllPath").val(),
+		  				"viewname":$("#viewname").val(),
+		  				"notes":$("#notes").val()	  				
+		  			}
+		  			Rose.ajax.postJson(srvMap.get('addMenu'), cmd, function(json, status) {
+						if(status) {
+							Operate_state = "update";
+							alert("保存成功！");
+						}
+	  				});
+		  		}
+		  		else{
+					var cmd = {
+						"funcId":currentMenu,
+		  				"funcCode":$("#funcCode").val(),
+		  				"name":$("#name").val(),
+		  				"funcImg":$("#funcImg").val(),
+						"funcType":$("#funcType option:selected").text(),
+		  				"funcArg":$("#funcArg").val(),
+		  				"dllPath":$("#dllPath").val(),
+		  				"viewname":$("#viewname").val(),
+		  				"notes":$("#notes").val()  				
+		  			}
+		  			Rose.ajax.postJson(srvMap.get('updateMenu'), cmd, function(json, status) {
+						if(status) {
+							alert("保存成功！");
+						}
+	  				});		  			
+
+		  		}
+	  		})
+
+	  		//删除
+			$("#menuDele").bind('click',function(){
+					var cmd = {
+						"funcId":currentMenu  				
+		  			}
+					Rose.ajax.postJson(srvMap.get('deleMenu'), cmd, function(json, status) {
+						if(status) {
+							alert("删除成功！");
+							
+							$("#funcCode").val("");
+			  				$("#name").val("");
+			  				$("#funcImg").val(""),
+							$("#funcType option:selected").text("");
+			  				$("#funcArg").val("");
+			  				$("#dllPath").val("");
+			  				$("#viewname").val("");
+			  				$("#notes").val("");							
+						}
+	  				});		  							
+	  		})	  		
+
+
+			/*// 首页菜单折叠
+		    $("#JS_toggleMenu").on('click', function () {
+		      if (!$('body').hasClass('sidebar-collapse')){
+		      	$('body').addClass("sidebar-collapse")
+		      }else{
+		      	$('body').removeClass("sidebar-collapse")
+		      }
+		    });
+*/
+		    
+
+			/*Rose.ajax.getJson(srvMap.get('myLinks'), '', function(json, status) {
+				if(status) {
+					var template = Handlebars.compile(Mtpl.myLinks);
+            		$myLinks.html(template(json.data));
+				}
+	  		});
+
+	  		Rose.ajax.getJson(srvMap.get('myMenus'), '', function(json, status) {
+				if(status) {
+					var template = Handlebars.compile(Mtpl.myMenus);
+            		$myMenus.html(template(json.data));
+				}
+	  		});*/
+		},
+	};
+	module.exports = MenuMng;
+});
+
---
+++ aiga3/src/main/webapp/html/tpl/MenuMng/getMenuinfo.tpl
@@ -0,0 +1,51 @@
+<form>
+	<div class="row">
+	    <div class="col-md-4">
+	      <div class="form-group">
+	        <label>功能编码</label>
+	        <label style="color: red">*</label>
+	        <input  class="form-control" id="funcCode" value="{{funcCode}}">
+	      </div>
+	      <div class="form-group">
+	        <label>图标路径</label>
+	        <input  class="form-control" id="funcImg" value="{{funcImg}}">
+	      </div>
+		 
+		  <div class="form-group">
+	        <label>模块参数</label>
+	        <input  class="form-control" id="funcArg" value="{{funcArg}}">
+	      </div>
+		  
+		</div>
+	<!-- /.col -->
+	    <div class="col-md-4">
+	      <div class="form-group">
+	        <label>功能名称</label>
+	        <label style="color: red">*</label>
+	        <input  class="form-control" id="name" value="{{name}}">
+	      </div>
+	      <div class="form-group">
+	        <label>模块类型</label>
+	        <label style="color: red">*</label>
+	        <select  id="funcType" class="form-control select2" style="width: 100%;">
+	          <option selected="true">{{funcType}}</option>
+	          <option></option>
+	        </select>
+	      </div>
+
+		  <div class="form-group">
+	        <label>dll文件名</label>
+	        <input  class="form-control" id="dllPath" value="{{dllPath}}">
+	      </div>
+		</div>
+	</div>
+		<div class="form-group">
+	        <label>链接地址</label>
+	        <input  class="form-control" id="viewname" value="{{viewname}}">
+	      </div>
+		  <div class="form-group">
+	        <label>备注</label>
+	        <input  class="form-control" id="notes" value="{{notes}}">
+	      </div>								
+<!-- /.row -->
+</form>

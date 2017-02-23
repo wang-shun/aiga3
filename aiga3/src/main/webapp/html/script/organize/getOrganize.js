48e4f45963b22b872e9f9b65b82f410f24b80792
lixs <lixs5@asiainfo.com>
2017-02-23 20:19:00 +0800

    no message

--- aiga3/src/main/webapp/html/script/organize/getOrganize.js
+++ aiga3/src/main/webapp/html/script/organize/getOrganize.js
@@ -1,4 +1,4 @@
-define(function(require,exports,module){
+define(function(require, exports, module) {
 
 	//引入公用模块
 	require('global/header.js');
@@ -26,105 +26,106 @@ define(function(require,exports,module){
 	//srvMap.add("queryUserinfoList", "home/queryUserinfoList.json", "/sys/role/query");
 
 	// 模板对象
-    var Tpl = {
-        getOrganize: require('tpl/organize/getOrganize.tpl')
-    };
+	var Tpl = {
+		getOrganize: require('tpl/organize/getOrganize.tpl')
+	};
 
 
-    // 容器对象
-    var Mod = {
-        organizeinfo: $('#organizeinfo')
-    };
+	// // 容器对象
+	// var D = {
+	// 	organizeinfo: $('#organizeinfo')
+	// };
 
-    //操作状态
-    var Operate_state = "update";
+	//操作状态
+	var Operate_state = "update";
 
-    var Data = {
-    	"organizeId":null,
-    	getUserinfoForm: '#JS_getOrganizeForm'
-    }
+	var Dom = {
+		organizeId: null,
+		getUserinfoForm: '#JS_getOrganizeForm',
+		getOrganize: '#organizeinfo'
+	}
 
+
+	var setting = {
+		check: {
+			enable: true
+		},
+		data: {
+			key: {
+				name: "organizeName"
+			},
+			simpleData: {
+				enable: true,
+				idKey: "organizeId",
+				pIdKey: "parentOrganizeId"
+			}
+		},
+		callback: {
+			onClick: function(event, treeId, treeNode) {
+				Dom.organizeId = treeNode.organizeId;
+				alert(treeNode.organizeName);
+				alert(treeNode.organizeId);
+				alert(treeNode.parentOrganizeId);
+				var cmd = {
+					"organizeId": Dom.organizeId
+				}
+				var cmd1 = {
+					"organizeType": "organizeType"
+				}
+				var sflxDataArray = [];
+				Rose.ajax.getJson(srvMap.get('constantOrganize'), cmd1, function(json, status) {
+					if (status) {
+						sflxDataArray = json.data;
+					}
+				});
+
+				Rose.ajax.getJson(srvMap.get('getOrganize'), cmd, function(json, status) {
+					if (status) {
+						var template = Handlebars.compile(Tpl.getOrganize);
+						console.log(json.data)
+						json.data["sflxDataArray"] = sflxDataArray;
+						console.log(json.data)
+						$(Dom.getOrganize).html(template(json.data));
+
+					}
+				});
+			}
+		}
+	};
 
 	var getOrganize = {
-		init: function(){
+		init: function() {
 			this._render();
 		},
 		_render: function() {
+			this.initOrganize();
+			this.organizeSave();
+			this.organizeDele();
+			this.organizeAdd();
 
-
-		var setting = {
-			check: {
-				enable: true
-			},
-			data: {
-				key: {
-				name:"organizeName"
-				},
-				simpleData: {
-					enable: true,
-					idKey:"organizeId",
-					pIdKey:"parentOrganizeId"
-				}
-			},
-			callback:{
-				 onClick: function(event, treeId, treeNode){
-					Data.organizeId = treeNode.organizeId;
-			        alert(treeNode.organizeName);
-					alert(treeNode.organizeId);
-			        alert(treeNode.parentOrganizeId);
-			        var cmd = {
-			        	"organizeId" : Data.organizeId
-			        }
-			        var cmd1 = {
-			        	"organizeType" : "organizeType"
-			        }
-			        var sflxDataArray = [];
-			        Rose.ajax.getJson(srvMap.get('constantOrganize'), cmd1, function(json, status) {
-			        	if (status) {
-			        		sflxDataArray = json.data;
-			        	}
-			        });
-			        
-			        Rose.ajax.getJson(srvMap.get('getOrganize'), cmd, function(json, status) {
-						if(status) {
-							var template = Handlebars.compile(Tpl.getOrganize);
-							console.log(json.data)
-							json.data["sflxDataArray"] = sflxDataArray;
-							console.log(json.data);
-            				Mod.organizeinfo.html(template(json.data));
-				    		
-						}
-					});
-				 }
-			}
-		};
-
-
-
+			// this.organizeSave();
 			Rose.ajax.getJson(srvMap.get('organizeTree'), '', function(json, status) {
-				if(status) {
+				if (status) {
 					console.log(json.data)
 					$.fn.zTree.init($("#treeDemo"), setting, json.data);
 				}
 			});
 
+		},
+		//////////////////
+		initOrganize: function() {
+			Rose.ajax.getJson(srvMap.get('getOrganize'), null, function(json, status) {
+				if (status) {
+					var template = Handlebars.compile(Tpl.getOrganize);
+					console.log(json.data)
+					$(Dom.getOrganize).html(template());
 
-			// Rose.ajax.getJson(srvMap.get('getOrganize'), '', function(json, status) {
-			// 	if(status) {
-			// 		var template = Handlebars.compile(Tpl.getOrganize);
-			// 		console.log(json.data)
-   //          		Mod.organizeinfo.html(template(json.data));
-   //          		 //iCheck
-			// 	    $('input[type="checkbox"].minimal, input[type="radio"].minimal').iCheck({
-			// 	      checkboxClass: 'icheckbox_minimal-blue',
-			// 	      radioClass: 'iradio_minimal-blue'
-			// 	    });
-					
-			// 	}
-	  // 		});
-			
-
-			$("#organizeAdd").bind('click',function(){
+				}
+			});
+		},
+		//新增
+		organizeAdd: function() {
+			$("#organizeAdd").bind('click', function() {
 
 				Operate_state = "new";
 
@@ -146,97 +147,98 @@ define(function(require,exports,module){
 				$("#connectBillId").val("");
 
 			});
+		},
 
-			//保存
-			$("#organizeSave").bind('click',function(){
+		//保存
+			organizeSave: function() {
+			$("#organizeSave").bind('click', function() {
 
-				if(Operate_state == "new"){
-		  			var cmd = {
-		  				"organizeId":organizeId,
-		  				"organizeName":$("#organizeName").val(),
-		  				"districtId":$("#districtId").val(),
-		  				"memberNum":$("#memberNum").val(),
-		  				"phoneId":$("#phoneId").val(),
-		  				"connectCardType":$("#connectCardType option:selected").text(),
-		  				"faxId":$("#faxId").val(),
-		  				"code":$("#code").val(),
-		  				"shortName":$("#shortName").val(),
-		  				"managerName":$("#managerName").val(),
-		  				"connectCardId":$("#connectCardId").val(),
-		  				"isLeaf":$("#isLeaf option:selected").text(),
-		  				"orgRoleTypeId":$("#orgRoleTypeId option:selected").text(),
-		  				"englishName":$("#englishName").val(),
-		  				"email":$("#email").val(),
-		  				"connectName":$("#connectName").val(),
-		  				"connectBillId":$("#connectBillId").val(),	  				
-		  			}
-		  			Rose.ajax.postJson(srvMap.get('saveOrganize'), cmd, function(json, status) {
-						if(status) {
+				if (Operate_state == "new") {
+					var cmd = {
+						"organizeId": organizeId,
+						"organizeName": $("#organizeName").val(),
+						"districtId": $("#districtId").val(),
+						"memberNum": $("#memberNum").val(),
+						"phoneId": $("#phoneId").val(),
+						"connectCardType": $("#connectCardType option:selected").text(),
+						"faxId": $("#faxId").val(),
+						"code": $("#code").val(),
+						"shortName": $("#shortName").val(),
+						"managerName": $("#managerName").val(),
+						"connectCardId": $("#connectCardId").val(),
+						"isLeaf": $("#isLeaf option:selected").text(),
+						"orgRoleTypeId": $("#orgRoleTypeId option:selected").text(),
+						"englishName": $("#englishName").val(),
+						"email": $("#email").val(),
+						"connectName": $("#connectName").val(),
+						"connectBillId": $("#connectBillId").val(),
+					}
+					Rose.ajax.postJson(srvMap.get('saveOrganize'), cmd, function(json, status) {
+						if (status) {
 							Operate_state = "update";
 							alert("保存成功！");
 						}
-	  				});
-	  			}else{
-	  				var cmd = {
-		  				"organizeId":organizeId,
-		  				"organizeName":$("#organizeName").val(),
-		  				"districtId":$("#districtId").val(),
-		  				"memberNum":$("#memberNum").val(),
-		  				"phoneId":$("#phoneId").val(),
-		  				"connectCardType":$("#connectCardType option:selected").text(),
-		  				"faxId":$("#faxId").val(),
-		  				"code":$("#code").val(),
-		  				"shortName":$("#shortName").val(),
-		  				"managerName":$("#managerName").val(),
-		  				"connectCardId":$("#connectCardId").val(),
-		  				"isLeaf":$("#isLeaf option:selected").text(),
-		  				"orgRoleTypeId":$("#orgRoleTypeId option:selected").text(),
-		  				"englishName":$("#englishName").val(),
-		  				"email":$("#email").val(),
-		  				"connectName":$("#connectName").val(),
-		  				"connectBillId":$("#connectBillId").val(),	  				
-		  			}
-		  			Rose.ajax.postJson(srvMap.get('updateOrganize'), cmd, function(json, status) {
-						if(status) {
+					});
+				} else {
+					var cmd = {
+						"organizeId": organizeId,
+						"organizeName": $("#organizeName").val(),
+						"districtId": $("#districtId").val(),
+						"memberNum": $("#memberNum").val(),
+						"phoneId": $("#phoneId").val(),
+						"connectCardType": $("#connectCardType option:selected").text(),
+						"faxId": $("#faxId").val(),
+						"code": $("#code").val(),
+						"shortName": $("#shortName").val(),
+						"managerName": $("#managerName").val(),
+						"connectCardId": $("#connectCardId").val(),
+						"isLeaf": $("#isLeaf option:selected").text(),
+						"orgRoleTypeId": $("#orgRoleTypeId option:selected").text(),
+						"englishName": $("#englishName").val(),
+						"email": $("#email").val(),
+						"connectName": $("#connectName").val(),
+						"connectBillId": $("#connectBillId").val(),
+					}
+					Rose.ajax.postJson(srvMap.get('updateOrganize'), cmd, function(json, status) {
+						if (status) {
 							alert("保存成功！");
 						}
-	  				});
-	  			}
+					});
+				}
 
 			});
-
-
-			//删除
-			$("#organizeDele").bind('click',function(){
-					var cmd = {
-						"organizeId":organizeId,  				
-		  			}
-					Rose.ajax.postJson(srvMap.get('deleOrganize'), cmd, function(json, status) {
-						if(status) {
-							alert("删除成功！");
-
-							$("#organizeName").val("");
-							$("#districtId").val("");
-							$("#memberNum").val("");
-							$("#phoneId").val("");
-							$("#connectCardType option:selected").text("");
-							$("#faxId").val("");
-							$("#code").val("");
-							$("#shortName").val("");
-							$("#managerName").val("");
-							$("#connectCardId").val("");
-							$("#isLeaf option:selected").text("");
-							$("#orgRoleTypeId option:selected").text("");
-							$("#englishName").val("");
-							$("#email").val("");
-							$("#connectName").val("");
-							$("#connectBillId").val("");	
-						}
-	  				});		  							
-	  		})
-	  	
 		},
+		organizeDele: function() {
+			//删除
+			$("#organizeDele").bind('click', function() {
+				var cmd = {
+					"organizeId": organizeId,
+				}
+				Rose.ajax.postJson(srvMap.get('deleOrganize'), cmd, function(json, status) {
+					if (status) {
+						alert("删除成功！");
+
+						$("#organizeName").val("");
+						$("#districtId").val("");
+						$("#memberNum").val("");
+						$("#phoneId").val("");
+						$("#connectCardType option:selected").text("");
+						$("#faxId").val("");
+						$("#code").val("");
+						$("#shortName").val("");
+						$("#managerName").val("");
+						$("#connectCardId").val("");
+						$("#isLeaf option:selected").text("");
+						$("#orgRoleTypeId option:selected").text("");
+						$("#englishName").val("");
+						$("#email").val("");
+						$("#connectName").val("");
+						$("#connectBillId").val("");
+					}
+				});
+			})
+		}
+
 	};
 	module.exports = getOrganize;
 });
-
--- aiga3/src/main/webapp/html/tpl/organize/getOrganize.tpl
+++ aiga3/src/main/webapp/html/tpl/organize/getOrganize.tpl
@@ -1,28 +1,26 @@
-<form>
+
 	<div class="row">
 	    <div class="col-md-4">
 	      <div class="form-group">
 	        <label>组织名称</label>
 	        <label style="color: red">*</label>
-	        <input  class="form-control" name="organizeName" value="{{organizeName}}">
+	        <input  class="form-control" id="organizeName" name="organizeName" value="{{organizeName}}">
 	      </div>
 	      <div class="form-group">
 	        <label>归属地区</label>
-	        <input  class="form-control" name="districtId" value="{{districtId}}">
+	        <input  class="form-control" id="districtId" value="{{districtId}}">
 	      </div>
 	      <div class="form-group">
 	        <label>人数</label>
-	        <input  class="form-control" name="memberNum" value="{{memberNum}}">
+	        <input  class="form-control" id="memberNum" value="{{memberNum}}">
 	      </div>
 	      <div class="form-group">
 	        <label>联系电话</label>
-	        <input  class="form-control" name="phoneId" value="{{phoneId}}">
+	        <input  class="form-control" id="phoneId" value="{{phoneId}}">
 	      </div>
 	      <div class="form-group">
 	        <label>联系人证件类型</label>
-	        <label style="color: red">*</label>
-	        
-	        <select  name="connectCardType" class="form-control select2" style="width: 100%;">
+	        <select id="connectCardType"  name="connectCardType" class="form-control select2" style="width: 100%;">
 	        	<option selected="true">{{connectCardType}}</option>
 	        	{{#each sflxDataArray}}
 	          <option value="{{value}}">{{show}}</option>
@@ -32,7 +30,7 @@
 	      </div>
 	      <div class="form-group">
 	        <label>传真</label>
-	        <input  class="form-control" name="faxId" value="{{faxId}}">
+	        <input  class="form-control" id="faxId" value="{{faxId}}">
 	      </div>
 		</div>
 		<!--2222222222222222222222222222222222222222222 -->
@@ -40,24 +38,24 @@
 	      <div class="form-group">
 	        <label>编码</label>
 	        <label style="color: red">*</label>
-	        <input  class="form-control" name="code" value="{{code}}">
+	        <input  class="form-control" id="code" name="code" value="{{code}}">
 	      </div>
 	      <div class="form-group">
 	        <label>简称</label>
-	        <input  class="form-control" name="shortName" value="{{shortName}}">
+	        <input  class="form-control" id="shortName" value="{{shortName}}">
 	      </div>
 	      <div class="form-group">
 	        <label>负责人名称</label>
-	        <input  class="form-control" name="managerName" value="{{managerName}}">
+	        <input  class="form-control" id="managerName" value="{{managerName}}">
 	      </div>
 	      
 	      <div class="form-group">
 	        <label>联系人证件号码</label>
-	        <input  class="form-control" name="connectCardId" value="{{connectCardId}}">
+	        <input  class="form-control" id="connectCardId" value="{{connectCardId}}">
 	      </div>
 	      <div class="form-group">
 	        <label>是否叶子</label>
-	        <select  name="isLeaf" class="form-control select2" style="width: 100%;">
+	        <select  id="isLeaf" class="form-control select2" style="width: 100%;">
 	          <option selected="true">{{isLeaf}}</option>
 	        </select>
 	      </div>
@@ -66,28 +64,27 @@
 	    <div class="col-md-4">
 	      <div class="form-group">
 	        <label>组织类型</label>
-	        <select  name="orgRoleTypeId" class="form-control select2" style="width: 100%;">
+	        <select  id="orgRoleTypeId" class="form-control select2" style="width: 100%;">
 	          <option selected="true">{{orgRoleTypeId}}</option>
 	        </select>
 	      </div>
 	      <div class="form-group">
 	        <label>英文名称</label>
-	        <input  class="form-control" name="englishName" value="{{englishName}}">
+	        <input  class="form-control" id="englishName" value="{{englishName}}">
 	      </div>
 		  <div class="form-group">
 	        <label>email</label>
-	        <input  class="form-control" name="email" value="{{email}}">
+	        <input  class="form-control" id="email" value="{{email}}">
 	      </div>
 	      <div class="form-group">
 	        <label>联系人名称</label>
 	        <label style="color: red">*</label>
-	        <input  class="form-control" name="connectName" value="{{connectName}}">
+	        <input  class="form-control" id="connectName" value="{{connectName}}">
 	      </div>
 	      <div class="form-group">
 	        <label>联系人手机号</label>
-	        <input  class="form-control" name="connectBillId" value="{{connectBillId}}">
+	        <input  class="form-control" id="connectBillId" value="{{connectBillId}}">
 	      </div>
 		</div>
 	</div>							
 <!-- /.row -->
-</form>

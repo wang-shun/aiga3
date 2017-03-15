ee9fb1ed76f27116648b6850f787bdf5638be85e
Ying <binhdu@foxmail.com>
2017-03-15 18:01:42 +0800

    功能模块菜单删除提示

--- aiga3/src/main/webapp/html/script/systemManage/menuManage.js
+++ aiga3/src/main/webapp/html/script/systemManage/menuManage.js
@@ -167,24 +167,27 @@ define(function(require,exports,module){
 				console.log(cmd);
 				Rose.ajax.postJson(srvMap.get('deleMenu')+"?"+cmd, '', function(json, status) {
 					if(status) {
-						self.initMenuList();
-						OperateState = null;
-						currentMenu = 0;
-						XMS.msgbox.show('删除菜单成功！', 'success', 3000)
-						$("#funcCode").val("");
-						$("#name").val("");
-						$("#funcImg").val("");
-						$("#funcType").val("");
-						$("#funcArg").val("");
-						$("#dllPath").val("");
-						$("#viewname").val("");
-						$("#notes").val("");
-						console.log(json.data);
-
-						_form.find('button[name="del"]').attr({"disabled":"disabled"});
-						_form.find('button[name="save"]').attr({"disabled":"disabled"});
-						// _form.find('button[name="save"]').removeAttr("disabled");//将按钮可用
-						// _form.find('button[name="del"]').removeAttr("disabled");//将按钮可用
+						if(json.retCode==200){
+							self.initMenuList();
+							OperateState = null;
+							currentMenu = 0;
+							XMS.msgbox.show('删除菜单成功！', 'success', 3000)
+							$("#funcCode").val("");
+							$("#name").val("");
+							$("#funcImg").val("");
+							$("#funcType").val("");
+							$("#funcArg").val("");
+							$("#dllPath").val("");
+							$("#viewname").val("");
+							$("#notes").val("");
+							console.log(json.data);
+							_form.find('button[name="del"]').attr({"disabled":"disabled"});
+							_form.find('button[name="save"]').attr({"disabled":"disabled"});
+							// _form.find('button[name="save"]').removeAttr("disabled");//将按钮可用
+							// _form.find('button[name="del"]').removeAttr("disabled");//将按钮可用
+						}else{
+							XMS.msgbox.show(json.retMessage, 'error', 3000)
+						}
 					}
 				});
 								

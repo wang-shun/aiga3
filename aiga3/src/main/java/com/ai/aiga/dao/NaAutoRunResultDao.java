9be97dc18fb4c6615d70c52a25769804b9e4b16a
dch <dch@person>
2017-03-17 16:06:16 +0800

    自动化执行结果基本信息

---
+++ aiga3/src/main/java/com/ai/aiga/dao/NaAutoRunResultDao.java
@@ -0,0 +1,9 @@
+package com.ai.aiga.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ai.aiga.domain.NaAutoRunResult;
+
+public interface NaAutoRunResultDao extends JpaRepository<NaAutoRunResult, Long>{
+
+}

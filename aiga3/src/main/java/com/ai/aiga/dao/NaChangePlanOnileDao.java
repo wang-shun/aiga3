5669e345498b61ec39695839891d827c0505ec30
asdf <asdf@hp>
2017-03-30 16:28:33 +0800

    变更

--- aiga3/src/main/java/com/ai/aiga/dao/NaChangePlanOnileDao.java
+++ aiga3/src/main/java/com/ai/aiga/dao/NaChangePlanOnileDao.java
@@ -3,6 +3,8 @@ package com.ai.aiga.dao;
 import java.math.BigDecimal;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import com.ai.aiga.dao.jpa.SearchAndPageRepository;
 import com.ai.aiga.domain.NaAutoMachine;
@@ -11,5 +13,13 @@ import com.ai.aiga.domain.NaChangePlanOnile;
 
 public interface NaChangePlanOnileDao extends JpaRepository<NaChangePlanOnile, Long>,SearchAndPageRepository<NaChangePlanOnile, Long> 
 {
-
+	@Modifying
+    @Query(value="update na_change_plan_onile set plan_state=4 where online_plan=?1",nativeQuery = true)
+	void delectChangePlanOnile(Long onlinePlan);
+	@Modifying
+    @Query(value="update na_change_plan_onile set sign=1 where online_plan=?1",nativeQuery = true)
+	void abandonChangePlanOnile(Long onlinePlan);
+	@Query(value="select online_plan,online_plan_name,plan_state,types,plan_date,timely,done_date"
+			+ "from na_change_plan_onile where online_plan=?1",nativeQuery = true)
+	NaChangePlanOnile findById(Long onlinePlan);
 }

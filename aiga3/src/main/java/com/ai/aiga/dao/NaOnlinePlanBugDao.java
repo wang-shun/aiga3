f3a8077b19899c83302dec17ec1f4954c0f1ba15
dch <dch@person>
2017-04-05 10:35:27 +0800

    故障異常基本信息

---
+++ aiga3/src/main/java/com/ai/aiga/dao/NaOnlinePlanBugDao.java
@@ -0,0 +1,13 @@
+package com.ai.aiga.dao;
+
+/**
+ * @ClassName: NaOnlinePlanBugDao
+ * @author: dongch
+ * @date: 2017年4月5日 上午10:25:54
+ * @Description:
+ * 
+ */
+public interface NaOnlinePlanBugDao {
+
+}
+

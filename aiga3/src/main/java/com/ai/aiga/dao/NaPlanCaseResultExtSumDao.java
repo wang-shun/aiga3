d5a477ee4bea05f77d356227846a20017c1cacdd
dch <dch@person>
2017-04-11 16:41:37 +0800

    dongch

--- aiga3/src/main/java/com/ai/aiga/dao/NaPlanCaseResultExtSumDao.java
+++ aiga3/src/main/java/com/ai/aiga/dao/NaPlanCaseResultExtSumDao.java
@@ -17,6 +17,10 @@ public interface NaPlanCaseResultExtSumDao extends SearchAndPageRepository<NaPla
     @Modifying
     @Query(value="update NA_PLAN_CASE_RESULT_EXP_SUM set operat_Id = ?1 where result_id= ?2 ", nativeQuery=true)
     public void saveOperatId(Long operatId , Long resultId);
+
+	@Modifying
+	@Query(value = "delete from na_plan_case_result_exp_sum where sub_task_id = ?1 and result_id in (?2)", nativeQuery = true)
+	void delete(Long taskId, List<Long> list);
     
 
     

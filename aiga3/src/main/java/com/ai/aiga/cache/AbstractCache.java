7d2d430259f84dc6c22e34757ed04bbe14151e4b
陶岳锋 <taoyf2012@gmail.com>
2017-04-13 15:39:34 +0800

    修改整体配置结构, 为后续各环境配置准备

--- aiga3/src/main/java/com/ai/aiga/cache/AbstractCache.java
+++ aiga3/src/main/java/com/ai/aiga/cache/AbstractCache.java
@@ -5,18 +5,26 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
 
 //暂时用map实现, 等下个阶段考虑用ehcache实现.
 public abstract class AbstractCache implements Icache, InitializingBean{
 	
 	protected Logger log = LoggerFactory.getLogger(getClass());
 	
+	private Object monitor = new Object();
+	
 	private Map cacheMap;
 	
 	protected abstract Map load();
 	
+	@Value("${app.cache.init}")
+	private String init;
+	
 	public void afterPropertiesSet() throws Exception{
-		reload();
+		if(Boolean.TRUE.toString().equalsIgnoreCase(init)){
+			reload();
+		}
 	}
 	
 	public final void reload(){
@@ -24,10 +32,28 @@ public abstract class AbstractCache implements Icache, InitializingBean{
 	}
 	
 	public Object getValue(Object key){
+		if(cacheMap == null){
+			synchronized (monitor) {
+				if(cacheMap == null){
+					reload();
+				}
+			}
+		}
+		
 		return cacheMap.get(key);
 	}
 	
 	public final Map getAll(){
+		if(cacheMap == null){
+			synchronized (monitor) {
+				if(cacheMap == null){
+					reload();
+				}
+			}
+		}
+		
 		return cacheMap;
 	}
+	
+	
 }

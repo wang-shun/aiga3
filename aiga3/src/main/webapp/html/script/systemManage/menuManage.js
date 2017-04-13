a477adc08ac11d43c43e30c6f94b3355b9fa4fae
Ying <binhdu@foxmail.com>
2017-04-13 10:58:58 +0800

    功能模块管理代码更新：代码修改

--- aiga3/src/main/webapp/html/script/systemManage/menuManage.js
+++ aiga3/src/main/webapp/html/script/systemManage/menuManage.js
@@ -1,7 +1,6 @@
-
 define(function(require, exports, module) {
 
-
+	var Utils = require("global/utils.js");
 
 	// 初始化菜单列表
 	srvMap.add("getMenulist", "systemManage/menuManage/getMenulist.json", "sys/menu/list");
@@ -16,13 +15,16 @@ define(function(require, exports, module) {
 
 	// 模板对象
 	var Tpl = {
-		getMenuinfo: require('tpl/MenuMng/getMenuinfo.tpl')
+		getMenuinfo: $("#TPL_menuInfoForm").html()
 	};
 
 	// 容器对象
 	var Dom = {
-		getMenuinfo: '#Page_menuInfo',
-		getMenuinfoForm: '#JS_getMenuinfoForm'
+		getMenuinfo: '#JS_menuInfo',
+		getMenuinfoForm: '#JS_getMenuinfoForm',
+		getTree: "#JS_menuTree",
+
+		btnMenuAdd: "JS_btnMenuAdd",
 
 	};
 	//操作状态
@@ -45,11 +47,12 @@ define(function(require, exports, module) {
 		callback: {
 			onClick: function(event, treeId, treeNode) {
 				currentMenu = treeNode.funcId;
-				$('#menuSave').removeAttr("disabled"); //将按钮可用
-				$('#menuDel').removeAttr("disabled"); //将按钮可用			 	
+				var _form = $(Dom.getMenuinfoForm);
+				_form.find("[name='save']").removeAttr("disabled"); //将按钮可用
+				_form.find("[name='del']").removeAttr("disabled"); //将按钮可用			 	
 				if (!(currentMenu == 0)) {
 					var cmd = "funcId=" + currentMenu;
-					Rose.ajax.getJson(srvMap.get('getMenuinfo') + "?" + cmd, '', function(json, status) {
+					Rose.ajax.getJson(srvMap.get('getMenuinfo'), cmd, function(json, status) {
 						if (status) {
 							OperateState = "update";
 							var template = Handlebars.compile(Tpl.getMenuinfo);
@@ -59,14 +62,7 @@ define(function(require, exports, module) {
 						}
 					});
 				} else {
-					$("#funcCode").val("");
-					$("#name").val("");
-					$("#funcImg").val("");
-					$("#funcType").val("");
-					$("#funcArg").val("");
-					$("#dllPath").val("");
-					$("#viewname").val("");
-					$("#notes").val("");
+					Utils.resetForm(_form);
 				}
 			}
 		}
@@ -77,8 +73,6 @@ define(function(require, exports, module) {
 			this._render();
 		},
 		_render: function() {
-			$("#Js_contentWrapper").find('h1').html("功能模块管理");
-			$("#Js_contentWrapper").find('li.active').html("功能模块管理");
 			this.initMenuList();
 			this.menuAdd();
 			this.menuSave();
@@ -99,8 +93,8 @@ define(function(require, exports, module) {
 						funcId: 0,
 						name: "功能菜单"
 					});
-					$.fn.zTree.init($("#treeDemo"), setting, json.data);
-					var treeObj = $.fn.zTree.getZTreeObj("treeDemo");
+					$.fn.zTree.init($("#JS_menuTree"), setting, json.data);
+					var treeObj = $.fn.zTree.getZTreeObj("JS_menuTree");
 					console.log(treeObj);
 					var nodes = treeObj.getNodesByParam("funcId", "0", null);
 					console.log(nodes);
@@ -112,18 +106,12 @@ define(function(require, exports, module) {
 		},
 
 		menuAdd: function() {
-
-			$("#menuAdd").bind('click', function() {
-
+			$(Dom.btnMenuAdd).bind('click', function() {
 				OperateState = "new";
-				$("#funcCode").val("");
-				$("#name").val("");
-				$("#funcImg").val("");
-				$("#funcType").val("");
-				$("#funcArg").val("");
-				$("#dllPath").val("");
-				$("#viewname").val("");
-				$("#notes").val("");
+				var _form = $(Dom.getMenuinfoForm);
+				_form.find("[name='save']").removeAttr("disabled"); //将按钮可用
+				_form.find("[name='del']").removeAttr("disabled"); //将按钮可用                
+				Utils.resetForm(_form);
 			})
 		},
 
@@ -132,12 +120,8 @@ define(function(require, exports, module) {
 			var _form = $(Dom.getMenuinfoForm);
 			_form.find('button[name="save"]').bind('click', function() {
 				// 表单校验：成功后调取接口
-				var cmd = $("#JS_getMenuinfoForm").serialize();
-				var funcCode = $("#funcCode").val();
-				var name = $("#name").val();
-				var funcType = $("#funcType").val();
-				console.log(funcCode + "&" + name + "&" + funcType);
-				if (funcCode && name && funcType) {
+				self.checkForm(_form, function() {
+					var cmd = _form.serialize();
 					if (OperateState == "new") {
 						cmd = "parentId=" + currentMenu + "&" + cmd;
 						console.log(cmd);
@@ -158,11 +142,10 @@ define(function(require, exports, module) {
 								XMS.msgbox.show('修改菜单成功！', 'success', 3000)
 							}
 						});
-
 					}
-				} else {
-					XMS.msgbox.show('有未填写内容！', 'error', 3000)
-				};
+
+				});
+
 			});
 
 		},
@@ -182,14 +165,6 @@ define(function(require, exports, module) {
 						XMS.msgbox.show('删除菜单成功！', 'success', 3000)
 						_form.find("input").val("");
 						_form.find("select").val("");
-						// $("#funcCode").val("");
-						// $("#name").val("");
-						// $("#funcImg").val("");
-						// $("#funcType").val("");
-						// $("#funcArg").val("");
-						// $("#dllPath").val("");
-						// $("#viewname").val("");
-						// $("#notes").val("");
 						console.log(json.data);
 						_form.find('button[name="del"]').attr({
 							"disabled": "disabled"
@@ -205,6 +180,25 @@ define(function(require, exports, module) {
 				});
 
 			})
+		},
+		checkForm: function(objForm, callback) {
+			var state = true;
+			var text = '';
+			$(objForm).find(':input[required]')
+				.not(':button, :submit, :reset, :hidden').each(function() {
+					var _val = $.trim($(this).val());
+					var _text = $.trim($(this).parent().prev().text());
+					if (_val == null || _val == undefined || _val == '') {
+						state = false;
+						text = _text.replace(/\：/, '');
+						return false;
+					}
+				})
+			if (state) {
+				callback(state);
+			} else {
+				XMS.msgbox.show(text.trimStar() + '不能为空！', 'error', 2000);
+			}
 		},
 
 	};

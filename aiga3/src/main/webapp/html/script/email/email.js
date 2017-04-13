301b459825c1e7aaeb1eefa59fd47674ce495390
王宁 <wn19950101@126.com>
2017-04-13 21:22:03 +0800

    代码更新

--- aiga3/src/main/webapp/html/script/email/email.js
+++ aiga3/src/main/webapp/html/script/email/email.js
@@ -47,22 +47,61 @@ define(function(require, exports, module) {
                     var _subject = _form.find("[name='subject']").val();
                     var markupStr = $('#summernote').summernote('code');
                     var _content = encodeURI(markupStr);
-                    console.log(_content)
-                    var files = _form.find("[name='attachment']")[0].files;
-                    console.log(files);
-                    var fileList = [];
-                    for (i = 0; i < files.length; i++) {
-                        fileList.push(files[i]);    
+                    console.log(""+_subject);
+                    if (_subject != "") {
+                        console.log(_content)
+                        var files = _form.find("[name='attachment']")[0].files;
+                        console.log(files);
+                        var fileList = [];
+                        for (i = 0; i < files.length; i++) {
+                            fileList.push(files[i]);
+                        }
+                        console.log(fileList);
+                        //var cmd = "addressee=" + _addressee + "&ccList=" + _ccList + "&subject=" + _subject + "&content=" + markupStr + "&files=" + files;
+                        console.log(_ccList)
+                        if (_ccList == " " || _ccList == null) {
+                            var cmd = {
+                                addressee: _addressee,
+                                subject: _subject,
+                                content: _content,
+                                files: fileList
+                            };
+                            XMS.msgbox.show('数据加载中，请稍候...', 'loading');
+                            $.ajaxUpload({
+                                url: srvMap.get('send'),
+                                data: cmd,
+                                success: function(data, status, xhr) {
+                                    console.log(data);
+                                    if (status) {
+                                        window.XMS.msgbox.show('发送成功！', 'success', 2000);
+                                    }
+                                }
+                            });
+                        } else {
+                            var cmd = {
+                                addressee: _addressee,
+                                ccList: _ccList,
+                                subject: _subject,
+                                content: _content,
+                                files: fileList
+                            };
+                            XMS.msgbox.show('数据加载中，请稍候...', 'loading');
+                            $.ajaxUpload({
+                                url: srvMap.get('send'),
+                                data: cmd,
+                                success: function(data, status, xhr) {
+                                    console.log(data);
+                                    if (status) {
+                                        window.XMS.msgbox.show('发送成功！', 'success', 2000);
+                                    }
+                                }
+                            });
+                        }
+                    } else {
+                        window.XMS.msgbox.show('主题不能为空！', 'error', 2000);
+                        return;
                     }
-                    console.log(fileList);
-                    //var cmd = "addressee=" + _addressee + "&ccList=" + _ccList + "&subject=" + _subject + "&content=" + markupStr + "&files=" + files;
-                    var cmd = {
-                        addressee: _addressee,
-                        ccList: _ccList,
-                        subject: _subject,
-                        content: _content,
-                        files: fileList
-                    }
+
 
                     // XMS.msgbox.show('数据加载中，请稍候...', 'loading');
                     // Rose.ajax.postJsonUpload(srvMap.get('send'), cmd, function(json, status) {
@@ -70,17 +109,7 @@ define(function(require, exports, module) {
                     //         window.XMS.msgbox.show('发送成功！', 'success', 2000);
                     //     }
                     // });
-                    XMS.msgbox.show('数据加载中，请稍候...', 'loading');
-                    $.ajaxUpload({
-                        url: srvMap.get('send'),
-                        data: cmd,
-                        success: function(data, status, xhr) {
-                            console.log(data);
-                            if (status) {
-                                window.XMS.msgbox.show('发送成功！', 'success', 2000);
-                            }
-                        }
-                    });
+
                 })
             });
         },

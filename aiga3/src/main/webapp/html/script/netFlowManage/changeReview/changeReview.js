774d64a785560edb372229be3c0bdb6c4b7b5d79
王宁 <wn19950101@126.com>
2017-04-19 17:01:31 +0800

    渲染修改

--- aiga3/src/main/webapp/html/script/netFlowManage/changeReview/changeReview.js
+++ aiga3/src/main/webapp/html/script/netFlowManage/changeReview/changeReview.js
@@ -421,16 +421,16 @@ define(function(require, exports, module) {
                 }
             });
             Handlebars.registerHelper('getDealState', function(value, fn) {
-                if (value == "0") {
+                if (value == "1") {
                     return "新增";
                 }
-                if (value == "1") {
+                if (value == "2") {
                     return "处理中";
                 }
-                if (value == "2") {
+                if (value == "3") {
                     return "完成";
                 }
-                if (value == "3") {
+                if (value == "4") {
                     return "取消";
                 }
             });

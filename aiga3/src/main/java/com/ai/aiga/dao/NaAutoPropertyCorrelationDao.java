a26c9302e24ec72c8d25b71215884b07ab09131f
baofd <baofd@asiainfo.com>
2017-04-19 11:13:06 +0800

    备份进程

--- aiga3/src/main/java/com/ai/aiga/dao/NaAutoPropertyCorrelationDao.java
+++ aiga3/src/main/java/com/ai/aiga/dao/NaAutoPropertyCorrelationDao.java
@@ -2,10 +2,7 @@ package com.ai.aiga.dao;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.ai.aiga.dao.jpa.SearchAndPageRepository;
-import com.ai.aiga.domain.NaAutoPropertyConfig;
 import com.ai.aiga.domain.NaAutoPropertyCorrelation;
 
 public interface NaAutoPropertyCorrelationDao extends SearchAndPageRepository<NaAutoPropertyCorrelation, Long>{

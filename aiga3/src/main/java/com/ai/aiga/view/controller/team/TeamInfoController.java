36510429b47c0d1fa11ac64bd3c13d623c5f6947
陶岳锋 <taoyf2012@gmail.com>
2017-04-20 18:06:25 +0800

    修改team 提交

---
+++ aiga3/src/main/java/com/ai/aiga/view/controller/team/TeamInfoController.java
@@ -0,0 +1,69 @@
+package com.ai.aiga.view.controller.team;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.ai.aiga.constant.BusiConstant;
+import com.ai.aiga.service.team.TeamInfoSv;
+import com.ai.aiga.view.controller.team.dto.TeamInfoRequest;
+import com.ai.aiga.view.json.base.JsonBean;
+
+/**
+ * @ClassName: TeamInfoController
+ * @author: liujinfang
+ * @date: 2017年4月5日 上午11:10:42
+ * @Description:
+ * 
+ */
+@Controller
+public class TeamInfoController {
+
+	@Autowired
+	private TeamInfoSv teamInfoSv;
+
+	@RequestMapping(path = "/sys/team/save")
+	public @ResponseBody JsonBean save(TeamInfoRequest naTeamInfo) {
+		teamInfoSv.save(naTeamInfo);
+		return JsonBean.success;
+	}
+
+	@RequestMapping(path = "/sys/team/update")
+	public @ResponseBody JsonBean update(TeamInfoRequest naTeamInfo) {
+		teamInfoSv.update(naTeamInfo);
+		return JsonBean.success;
+	}
+
+	@RequestMapping(path = "/sys/team/del")
+	public @ResponseBody JsonBean del(@RequestParam Long teamId) {
+		teamInfoSv.delete(teamId);
+		return JsonBean.success;
+	}
+
+	@RequestMapping(path = "/sys/team/findone")
+	public @ResponseBody JsonBean findone(@RequestParam Long teamId) {
+		JsonBean bean = new JsonBean();
+		bean.setData(teamInfoSv.findone(teamId));
+		return bean;
+	}
+
+	@RequestMapping(path = "/sys/team/findByName")
+	public @ResponseBody JsonBean list(
+			@RequestParam(value = "page", defaultValue = BusiConstant.PAGE_DEFAULT + "") int pageNumber,
+			@RequestParam(value = "pageSize", defaultValue = BusiConstant.PAGE_DEFAULT + "") int pageSize,
+			TeamInfoRequest condition) {
+
+		JsonBean bean = new JsonBean();
+		bean.setData(teamInfoSv.list(pageNumber, pageSize, condition));
+		return bean;
+	}
+
+	@RequestMapping(path = "/sys/employeeandteam/saveemployee")
+	public @ResponseBody JsonBean save(String list, Long teamId) {
+		teamInfoSv.saveEnv(list, teamId);
+		return JsonBean.success;
+	}
+
+}
---
+++ aiga3/src/main/java/com/ai/aiga/view/controller/team/dto/TeamInfoRequest.java
@@ -0,0 +1,27 @@
+package com.ai.aiga.view.controller.team.dto;
+
+import java.util.Date;
+
+import lombok.Data;
+
+/**
+ * @ClassName: TeamInfoRequest
+ * @author: taoyf
+ * @date: 2017年4月20日 下午5:22:05
+ * @Description:
+ * 
+ */
+@Data
+public class TeamInfoRequest {
+	
+    private Long teamId;
+    private String teamType;
+    private String createOpId;
+    private Date createDate;
+    private String remark;
+    private String ext1;
+    private String ext2;
+    private String ext3;
+
+}
+

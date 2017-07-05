41a052b680b5f936466c0b4d11b7f38ac6d1ecef
dupeng <dupeng5@asiainfo.com>
2017-07-05 10:28:34 +0800

    柱状图模版

--- aiga3/src/main/webapp/html/script/sysArchiBaselineManage/archiChangeMessage.js
+++ aiga3/src/main/webapp/html/script/sysArchiBaselineManage/archiChangeMessage.js
@@ -41,11 +41,8 @@ define(function(require, exports, module) {
 			var self = this;
 			self._querydomain();
 		},
-
-		//查询下拉框数据加载，绑定查询按钮事件
-		_querydomain: function() {
-			var self = this;
-			var myChart = echarts.init(Page.findId('archiView')[0]);
+		
+		_graphSec: function(myChart) {
 			option = {
 			    tooltip : {
 			        trigger: 'axis',
@@ -140,23 +137,86 @@ define(function(require, exports, module) {
 			    ]
 			};
 			myChart.setOption(option);
-//			var _form = Page.findId('selectData');
-//			Utils.setSelectData(_form);
-//			var _queryBtn =  _form.find("[name='query']");
-//			_queryBtn.off('click').on('click',function(){
-//
-//				var _idFirst = _form.find("[name='primaryDomain']").val();
-//				var cmd= "idFirst="+_idFirst+"&level=122";
-//				Rose.ajax.postJson(srvMap.get("getSecView"),cmd, function(json, status) {
-//					if(status) {
-//						var template = Handlebars.compile(Tpl.getSecView);
-//		        		Page.findName("archiView").html(template(json.data));
-//
-//		        		//计算高度值
-//		        		self.setSidebarHeight();
-//					}
-//	  			});
-//			});
+		},
+		
+		_graphfir: function(myChart) {
+			option = {
+			    title : {
+			        text: '某地区蒸发量和降水量',
+			        subtext: '纯属虚构'
+			    },
+			    tooltip : {
+			        trigger: 'axis'
+			    },
+			    legend: {
+			        data:['蒸发','降水量']
+			    },
+			    toolbox: {
+			        show : true,
+			        feature : {
+			            dataView : {show: true, readOnly: false},
+			            magicType : {show: true, type: ['line', 'bar']},
+			            restore : {show: true},
+			            saveAsImage : {show: true}
+			        }
+			    },
+			    calculable : true,
+			    xAxis : [
+			        {
+			            type : 'category',
+			            data : ['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月']
+			        }
+			    ],
+			    yAxis : [
+			        {
+			            type : 'value'
+			        }
+			    ],
+			    series : [
+			        {
+			            name:'蒸发',
+			            type:'bar',
+			            data:[2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3],
+			            markPoint : {
+			                data : [
+			                    {type : 'max', name: '最大值'},
+			                    {type : 'min', name: '最小值'}
+			                ]
+			            },
+			            markLine : {
+			                data : [
+			                    {type : 'average', name: '平均值'}
+			                ]
+			            }
+			        },
+			        {
+			            name:'降水量',
+			            type:'bar',
+			            data:[2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3],
+			            markPoint : {
+			                data : [
+			                    {name : '年最高', value : 182.2, xAxis: 7, yAxis: 183},
+			                    {name : '年最低', value : 2.3, xAxis: 11, yAxis: 3}
+			                ]
+			            },
+			            markLine : {
+			                data : [
+			                    {type : 'average', name : '平均值'}
+			                ]
+			            }
+			        }
+			    ]
+			};
+			myChart.setOption(option);
+		},
+		//查询下拉框数据加载，绑定查询按钮事件
+		_querydomain: function() {
+			var self = this;
+			var myChart = echarts.init(Page.findId('archiView')[0]);
+			//柱状 模版
+			self._graphfir(myChart);
+			//柱状堆叠模版
+//			self._graphSec(myChart);
 		},
 		setSidebarHeight:function(){
 			Page.find('.mxgif-sidebar').each(function(){

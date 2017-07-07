223992cce4f31e3807a3c1ca83724630b8f16782
liuyang <liuyang19@asiainfo.com>
2017-07-07 14:56:20 +0800

    architecture question index name bug commit

--- aiga3/src/main/java/com/ai/aiga/dao/AmCoreIndexDao.java
+++ aiga3/src/main/java/com/ai/aiga/dao/AmCoreIndexDao.java
@@ -1,11 +1,15 @@
 package com.ai.aiga.dao;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import com.ai.aiga.dao.jpa.SearchAndPageRepository;
 import com.ai.aiga.domain.AmCoreIndex;
 
 public interface AmCoreIndexDao extends JpaRepository<AmCoreIndex, Long>, SearchAndPageRepository<AmCoreIndex, Long> {
 
-	
+	  //根据@Query中的sql执行
+	  @Query("select b from AmCoreIndex a, ArchDbConnect b where a.indexId = b.indexId")
+	  List<AmCoreIndex>findAllConnects();
 }

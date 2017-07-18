f39ca04d194f9ec6e3aad3b423423c1b02402102
dupeng <dupeng5@asiainfo.com>
2017-07-18 14:45:37 +0800

    no message

--- aiga3/src/main/webapp/html/script/sysArchiBaselineManage/archiChangeMessage.js
+++ aiga3/src/main/webapp/html/script/sysArchiBaselineManage/archiChangeMessage.js
@@ -35,8 +35,7 @@ define(function(require, exports, module) {
 				if(init) {
 					var date = self.formatDate(new Date()); 		
 					_cmd = 'beginTime='+date+'&endTime='+date;
-					init = false;
-			
+					init = false;			
 				}
 				if(_cmd.indexOf('beginTime=&')>-1) {
 					XMS.msgbox.show('请输入开始时间！', 'error', 2000);

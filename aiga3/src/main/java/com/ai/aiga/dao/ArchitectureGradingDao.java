0a937b36f7a2c705998233d1f44b02163121907b
dupeng <dupeng5@asiainfo.com>
2017-07-24 17:48:33 +0800

    grandingDao

--- aiga3/src/main/java/com/ai/aiga/dao/ArchitectureGradingDao.java
+++ aiga3/src/main/java/com/ai/aiga/dao/ArchitectureGradingDao.java
@@ -1,14 +1,9 @@
 package com.ai.aiga.dao;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-
 import com.ai.aiga.dao.jpa.SearchAndPageRepository;
 import com.ai.aiga.domain.ArchitectureGrading;
 
-public interface ArchitectureGradingDao extends SearchAndPageRepository<ArchitectureGrading, Long> {
+public interface ArchitectureGradingDao extends JpaRepository<ArchitectureGrading, Long>,SearchAndPageRepository<ArchitectureGrading, Long> {
 	
 }

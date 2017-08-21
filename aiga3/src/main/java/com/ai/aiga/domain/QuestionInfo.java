fc92d13c9f9ecb1a51375c6da703f6ecb8890182
liuyang <liuyang19@asiainfo.com>
2017-08-21 16:15:32 +0800

    questioninfo domain add seq

--- aiga3/src/main/java/com/ai/aiga/domain/QuestionInfo.java
+++ aiga3/src/main/java/com/ai/aiga/domain/QuestionInfo.java
@@ -6,9 +6,12 @@ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -106,8 +109,9 @@ public class QuestionInfo  implements java.io.Serializable {
        this.ext3 = ext3;
     }
    
-     @Id 
-    
+    @Id 
+    @GeneratedValue(strategy=GenerationType.SEQUENCE,generator="SEQ_QUES_INFO")
+    @SequenceGenerator(name="SEQ_QUES_INFO",sequenceName="SEQ_QUES_INFO",allocationSize=1)
     @Column(name="QUES_ID", unique=true, nullable=false, precision=10, scale=0)
     public long getQuesId() {
         return this.quesId;

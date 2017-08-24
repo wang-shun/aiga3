0cf955cb77bc557593f9ab8306b347e6c3966aa6
dupeng <dupeng5@asiainfo.com>
2017-08-24 17:10:01 +0800

    echars上线时间打点

---
+++ aiga3/src/main/java/com/ai/aiga/dao/ArchiOnlinePlanDao.java
@@ -0,0 +1,10 @@
+package com.ai.aiga.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ai.aiga.domain.ArchiOnlinePlan;
+
+
+public interface ArchiOnlinePlanDao extends JpaRepository<ArchiOnlinePlan, Long> {
+
+}

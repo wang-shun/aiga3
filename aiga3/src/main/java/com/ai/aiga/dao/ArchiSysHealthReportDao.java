4125b6d8c3554972652191c3abeeb5f8f4bbb2da
dupeng <dupeng5@asiainfo.com>
2017-08-25 17:05:13 +0800

    系统体检报告后台

---
+++ aiga3/src/main/java/com/ai/aiga/dao/ArchiSysHealthReportDao.java
@@ -0,0 +1,9 @@
+package com.ai.aiga.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ai.aiga.domain.ArchiSysHealthReport;
+
+public interface ArchiSysHealthReportDao extends JpaRepository<ArchiSysHealthReport, Long> {
+
+}
---
+++ aiga3/src/main/java/com/ai/aiga/service/ArchiSysHealthReportSv.java
@@ -0,0 +1,7 @@
+package com.ai.aiga.service;
+
+import com.ai.aiga.service.base.BaseService;
+
+public class ArchiSysHealthReportSv extends BaseService {
+
+}

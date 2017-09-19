a5e528e3ab9ed9b0224d501a545f4721ca9138a3
dupeng <dupeng5@asiainfo.com>
2017-09-19 10:06:24 +0800

    修改邮箱，修改问题跳页面

--- aiga3/src/main/webapp/html/script/welcome.js
+++ aiga3/src/main/webapp/html/script/welcome.js
@@ -49,28 +49,27 @@ define(function(require,exports,module){
                     eCont: 'JS_getWelcomePlanDate',
                     specialDates: specialDates,
                     onpicked: function(dp) {
-//                    	cache.clickTime++;
-//						setTimeout(function() {
-//							cache.clickTime--;
-//						}, 200);
-//						if(cache.clickTime>1) {
-//				            Rose.ajax.postJson(srvMap.get('onlineTimeSet'), 'onlineTime='+Rose.date.dateTime2str(new Date(dp.cal.getDateStr()), 'yyyy/MM/dd'), function(json, status) {
-//				                if (status) {
-//				                    XMS.msgbox.show(json.data.outputMessage, 'success', 1500);
-//				                    specialDatesInit(json.data.onlineDate);				                   
-//				                } else {
-//				    				XMS.msgbox.show(json.retMessage, 'error', 2000);
-//				                }
-//				            });
-//						} else {
-//	                        //alert('你选择的日期是:' + dp.cal.getDateStr())
-//	                        Data.planDate = dp.cal.getDateStr();
-//	                        Page.findName("showTime").html(Rose.date.dateTime2str(new Date(dp.cal.getDateStr()), 'yyyy年MM月dd日'));
-//						}
-
-                        //alert('你选择的日期是:' + dp.cal.getDateStr())
-                        Data.planDate = dp.cal.getDateStr();
-                        Page.findName("showTime").html(Rose.date.dateTime2str(new Date(dp.cal.getDateStr()), 'yyyy年MM月dd日'));
+                    	cache.clickTime++;
+						setTimeout(function() {
+							cache.clickTime--;
+						}, 200);
+						if(cache.clickTime>1) {
+				            Rose.ajax.postJson(srvMap.get('onlineTimeSet'), 'onlineTime='+Rose.date.dateTime2str(new Date(dp.cal.getDateStr()), 'yyyy/MM/dd'), function(json, status) {
+				                if (status) {
+				                    XMS.msgbox.show(json.data.outputMessage, 'success', 1500);
+				                    specialDatesInit(json.data.onlineDate);				                   
+				                } else {
+				    				XMS.msgbox.show(json.retMessage, 'error', 2000);
+				                }
+				            });
+						} else {
+	                        //alert('你选择的日期是:' + dp.cal.getDateStr())
+	                        Data.planDate = dp.cal.getDateStr();
+	                        Page.findName("showTime").html(Rose.date.dateTime2str(new Date(dp.cal.getDateStr()), 'yyyy年MM月dd日'));
+						}
+//                        //alert('你选择的日期是:' + dp.cal.getDateStr())
+//                        Data.planDate = dp.cal.getDateStr();
+//                        Page.findName("showTime").html(Rose.date.dateTime2str(new Date(dp.cal.getDateStr()), 'yyyy年MM月dd日'));
 					
                     }
                 });
@@ -115,9 +114,9 @@ define(function(require,exports,module){
                     	var quesId = $(this).attr("data-quesid");
                     	//问题解决页面
         				var objData = {
-        						id : '135',
-        						name : '架构问题判定',
-        						href : "view/archiQuesManage/quesRending.html",
+        						id : '136',
+        						name : '架构问题检索',
+        						href : "view/archiQuesManage/quesSearch.html",
         	                    cmd : "quesId="+quesId
         				};
                     	Tab.creatTab(objData);

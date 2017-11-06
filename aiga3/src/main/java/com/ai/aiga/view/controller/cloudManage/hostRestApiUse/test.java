2783dfe59de9a577b69acb89d1666d99c00a92c4
dupeng <dupeng5@asiainfo.com>
2017-11-06 14:39:18 +0800

    目录

---
+++ aiga3/src/main/java/com/ai/aiga/view/controller/cloudManage/hostRestApiUse/test.java
@@ -0,0 +1,13 @@
+package com.ai.aiga.view.controller.cloudManage.hostRestApiUse;
+
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

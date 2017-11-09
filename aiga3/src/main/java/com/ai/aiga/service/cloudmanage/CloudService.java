5c666ce4d2c32fdcaae1d6f1383e107da01c2342
dupeng <dupeng5@asiainfo.com>
2017-11-09 10:05:04 +0800

    代码重提

---
+++ aiga3/src/main/java/com/ai/aiga/service/cloudmanage/CloudService.java
@@ -0,0 +1,155 @@
+package com.ai.aiga.service.cloudmanage;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ai.aiga.service.base.BaseService;
+import com.ai.aiga.service.cloudmanage.dto.CloudFirstParam;
+import com.ai.aiga.service.cloudmanage.dto.CloudOutput;
+import com.ai.aiga.service.cloudmanage.dto.CloudSecondParam;
+import com.ai.aiga.service.cloudmanage.dto.CloudThirdParam;
+import com.ai.aiga.util.mapper.BeanMapper;
+import com.ai.aiga.view.controller.archiQuesManage.dto.ArchitectureFirstRequest;
+import com.ai.aiga.view.controller.archiQuesManage.dto.ArchitectureSecondRequest;
+import com.ai.aiga.view.controller.archiQuesManage.dto.ArchitectureThirdRequest;
+
+@Service
+@Transactional
+public class CloudService extends BaseService {
+	
+	public static void main(String[] args) {
+		ArchitectureFirstRequest param = new ArchitectureFirstRequest();
+		param.setApplyId(759L);
+		param.setApplyUser("admin");
+		param.setCode("数据修改");
+		param.setCreateDate(new Date());
+		param.setExt1("1");
+		param.setIdFirst(99000000L);
+		param.setModifyDate(new Date());
+		param.setName("一级域测试数据");
+		param.setState("审批通过");
+		System.out.println(new CloudService().firstDelete(param).getMessage());
+		//二级测试
+//		ArchitectureSecondRequest param = new ArchitectureSecondRequest();
+//		param.setApplyId(760L);
+//		param.setApplyUser("admin");
+//		param.setCode("数据修改");
+//		param.setCreateDate(new Date());
+//		param.setExt1("2");
+//		param.setIdFirst(10000000L);
+//		param.setIdSecond(19900000L);
+//		param.setBelongLevel("SaaS");
+//		param.setModifyDate(new Date());
+//		param.setName("二级域测试数据修改");
+//		param.setState("审批通过");
+//		System.out.println(new CloudService().secondDelete(param).getMessage());
+	}
+	
+	/**
+	 * 新增业务系统一级域同步
+	 * @param params
+	 * @return
+	 */
+	public CloudOutput firstAdd(ArchitectureFirstRequest params) {
+		CloudFirstParam cloudFirstParam = BeanMapper.map(params, CloudFirstParam.class);
+		cloudFirstParam.setCreateDate(String.valueOf(params.getCreateDate().getTime()));
+		cloudFirstParam.setModifyDate(String.valueOf(params.getModifyDate().getTime()));
+		return CloudServiceUtil.cloudRestfulcall("architecture/first/add",cloudFirstParam);
+	}
+	
+	/**
+	 * 修改业务系统一级域同步
+	 * @param params
+	 * @return
+	 */
+	public CloudOutput firstModify(ArchitectureFirstRequest params) {
+		CloudFirstParam cloudFirstParam = BeanMapper.map(params, CloudFirstParam.class);
+		cloudFirstParam.setCreateDate(String.valueOf(params.getCreateDate().getTime()));
+		cloudFirstParam.setModifyDate(String.valueOf(params.getModifyDate().getTime()));
+		return CloudServiceUtil.cloudRestfulcall("architecture/first/modify",cloudFirstParam);
+	}
+	
+	/**
+	 * 删除业务系统一级域同步
+	 * @param params
+	 * @return
+	 */
+	public CloudOutput firstDelete(ArchitectureFirstRequest params) {
+		CloudFirstParam cloudFirstParam = BeanMapper.map(params, CloudFirstParam.class);
+		cloudFirstParam.setCreateDate(String.valueOf(params.getCreateDate().getTime()));
+		cloudFirstParam.setModifyDate(String.valueOf(params.getModifyDate().getTime()));
+		return CloudServiceUtil.cloudRestfulcall("architecture/first/delete",cloudFirstParam);
+	}
+	/**
+	 * 新增业务系统二级域同步
+	 * @param params
+	 * @return
+	 */
+	public CloudOutput secondAdd(ArchitectureSecondRequest params) {
+		CloudSecondParam cloudSecondParam = BeanMapper.map(params, CloudSecondParam.class);
+		cloudSecondParam.setCreateDate(String.valueOf(params.getCreateDate().getTime()));
+		cloudSecondParam.setModifyDate(String.valueOf(params.getModifyDate().getTime()));
+		return CloudServiceUtil.cloudRestfulcall("architecture/second/add",cloudSecondParam);
+	}
+	
+	/**
+	 * 修改业务系统二级域同步
+	 * @param params
+	 * @return
+	 */
+	public CloudOutput secondModify(ArchitectureSecondRequest params) {
+		CloudSecondParam cloudSecondParam = BeanMapper.map(params, CloudSecondParam.class);
+		cloudSecondParam.setCreateDate(String.valueOf(params.getCreateDate().getTime()));
+		cloudSecondParam.setModifyDate(String.valueOf(params.getModifyDate().getTime()));
+		return CloudServiceUtil.cloudRestfulcall("architecture/second/modify",cloudSecondParam);
+	}
+	
+	/**
+	 * 删除业务系统二级域同步
+	 * @param params
+	 * @return
+	 */
+	public CloudOutput secondDelete(ArchitectureSecondRequest params) {
+		CloudSecondParam cloudSecondParam = BeanMapper.map(params, CloudSecondParam.class);
+		cloudSecondParam.setCreateDate(String.valueOf(params.getCreateDate().getTime()));
+		cloudSecondParam.setModifyDate(String.valueOf(params.getModifyDate().getTime()));
+		return CloudServiceUtil.cloudRestfulcall("architecture/second/delete",cloudSecondParam);
+	}
+	/**
+	 * 新增业务系统三级域同步
+	 * @param params
+	 * @return
+	 */
+	public CloudOutput thirdAdd(ArchitectureThirdRequest params) {
+		CloudThirdParam cloudThirdParam = BeanMapper.map(params, CloudThirdParam.class);
+		cloudThirdParam.setCreateDate(String.valueOf(params.getCreateDate().getTime()));
+		cloudThirdParam.setModifyDate(String.valueOf(params.getModifyDate().getTime()));
+		return CloudServiceUtil.cloudRestfulcall("architecture/third/add",cloudThirdParam);
+	}
+	
+	/**
+	 * 修改业务系统三级域同步
+	 * @param params
+	 * @return
+	 */
+	public CloudOutput thirdModify(ArchitectureThirdRequest params) {
+		CloudThirdParam cloudThirdParam = BeanMapper.map(params, CloudThirdParam.class);
+		cloudThirdParam.setCreateDate(String.valueOf(params.getCreateDate().getTime()));
+		cloudThirdParam.setModifyDate(String.valueOf(params.getModifyDate().getTime()));
+		return CloudServiceUtil.cloudRestfulcall("architecture/third/modify",cloudThirdParam);
+	}
+	
+	/**
+	 * 删除业务系统三级域同步
+	 * @param params
+	 * @return
+	 */
+	public CloudOutput thirdDelete(ArchitectureThirdRequest params) {
+		CloudThirdParam cloudThirdParam = BeanMapper.map(params, CloudThirdParam.class);
+		cloudThirdParam.setCreateDate(String.valueOf(params.getCreateDate().getTime()));
+		cloudThirdParam.setModifyDate(String.valueOf(params.getModifyDate().getTime()));
+		return CloudServiceUtil.cloudRestfulcall("architecture/third/delete",cloudThirdParam);
+	}
+}

35599aeae05fb92f02bf1fb819bb2c9afd783ea4
liuyang <liuyang19@asiainfo.com>
2017-12-01 16:37:09 +0800

    模板 补充

---
+++ aiga3/src/main/java/com/ai/aiga/view/controller/archiQuesManage/dto/TaskDispatchReport.java
@@ -0,0 +1,19 @@
+package com.ai.aiga.view.controller.archiQuesManage.dto;
+
+import java.io.Serializable;
+
+import lombok.Data;
+
+@Data
+public class TaskDispatchReport implements Serializable {
+
+    private String key1;
+    private String predayAddTaskNum;
+    private String residentTaskNum;
+    private String nonresidentTaskNum;
+    private String batchTaskNum;
+    private String predayTaskTriggerNum;
+    private String csfSuccessRateChainRatio;
+    private String settMonth;
+    
+}

4b61c884653fc85a7cca81bea08788e09b10ea47
zhuchao <zhuchao@asiainfo.com>
2018-06-12 15:23:45 +0800

    新接入业务数据库连接容量评估模型 菜单开发

--- aiga3/src/main/webapp/html/script/databaseConnectionCapacityEvaluation/capacityEvaluation.js
+++ aiga3/src/main/webapp/html/script/databaseConnectionCapacityEvaluation/capacityEvaluation.js
@@ -1,3 +1,21 @@
-/**
- * Created by zhuchao on 18-6-12.
- */
+define(function(require, exports, module) {
+    //引入icheck的js
+    require("icheck/js/icheck.min.js");
+    // 通用工具模块
+    var Utils = require("global/utils.js");
+    var pathAlias = "databaseConnectionCapacityEvaluation/";
+    // 初始化页面ID(和文件名一致)，不需要带'#Page_'
+    var Page = Utils.initPage('capacityEvaluation');
+    var init = {
+        init: function() {
+            $('input').iCheck({
+                labelHover : false,
+                cursor : true,
+                checkboxClass : 'icheckbox_square-blue',
+                radioClass : 'iradio_square-blue',
+                increaseArea : '20%'
+            });
+        }
+    };
+    module.exports = init;
+});

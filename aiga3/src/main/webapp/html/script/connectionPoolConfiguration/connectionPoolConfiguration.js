7b17fd5b9d69f0f1d9298b874b1a5918fed6d557
huyj <1321676632@qq.com>
2018-06-22 09:15:05 +0800

    no message

--- aiga3/src/main/webapp/html/script/connectionPoolConfiguration/connectionPoolConfiguration.js
+++ aiga3/src/main/webapp/html/script/connectionPoolConfiguration/connectionPoolConfiguration.js
@@ -113,19 +113,16 @@ define(function(require, exports, module) {
 	                    var desc = function (x, y) {
 	            	        return (x.centerName).localeCompare(y.centerName,"zh");
 	            	    };                    
-	                    _data.sort(desc);
                     }else if(namev == "module"){
                     	var desc = function (x, y) {
 	            	        return (x.module).localeCompare(y.module);
 	            	    };                    
-	                    _data.sort(desc);
                     }else if(namev == "db"){
                     	var desc = function (x, y) {
 	            	        return (x.db).localeCompare(y.db);
-	            	    };                    
-	                    _data.sort(desc);
+	            	    };                    	                    
                     }
-                    
+                    _data.sort(desc);
                     for (var i in _data) {
                     	var _json = _data[i];
                         var _key, _value;

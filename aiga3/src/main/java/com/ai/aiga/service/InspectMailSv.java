25a8327e3554aeadc520aef9bd26fd7b9a787020
dupeng <dupeng5@asiainfo.com>
2018-06-29 16:20:31 +0800

    样式调整

--- aiga3/src/main/java/com/ai/aiga/service/InspectMailSv.java
+++ aiga3/src/main/java/com/ai/aiga/service/InspectMailSv.java
@@ -80,7 +80,7 @@ public class InspectMailSv extends BaseService {
 			"<head>"+
 			"<div style='text-align: center;font-size: 16.0pt;font-weight: bold;border-bottom:1px solid #9b9999;padding-top:3px;padding-bottom:3px;'>"+dateString+"新业务上线次日系统运行情况巡检报告</div>"+
 			"<div style='font-family: 等线;'>&nbsp;</div>"+
-			"<div style='font-family: 等线;'><span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>本次架构治理共对"+bModuleNum+"大类"+sModuleNum+"小类完成新业务上线次日巡检，共发现"+dModuleNum+"小类存在异常波动，详细巡检结果见下文：</div>"+
+			"<div style='font-family: 等线;font-size: 14pt;font-weight: bold;'><span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>本次架构治理共对<span style='color:red'>"+bModuleNum+"</span>大类<span style='color:red'>"+sModuleNum+"</span>小类完成新业务上线次日巡检，共发现<span style='color:red'>"+dModuleNum+"</span>小类存在异常波动，详细巡检结果见下文：</div>"+
 			"<div style='font-family: 等线;'>&nbsp;</div>"+
 			"</head>");
 		//模块添加

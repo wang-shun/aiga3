89aef0657433a60c1f2740857ead79e96b87475c
liuyang <liuyang19@asiainfo.com>
2018-07-02 14:41:36 +0800

    null change .length bug commit

--- aiga3/src/main/java/com/ai/aiga/service/ArchIndex/ArchitectureIndexSv.java
+++ aiga3/src/main/java/com/ai/aiga/service/ArchIndex/ArchitectureIndexSv.java
@@ -111,7 +111,7 @@ public class ArchitectureIndexSv extends BaseService {
 		List<ParameterCondition>params = new ArrayList<ParameterCondition>();
 		long[] ids = null;
 		long id0,id1,id2,id3,id4,id5,id6,id7,id8,id9,id10,id11,id12,id13,id14,id15,id16,id17,id18,id19,id20,id21,id22,id23,id24,id25,id26,id27,id28,id29,id30,id31,id32,id33,id34,id35,id36,id37,id38,id39,id40,id41,id42,id43,id44,id45,id46,id47,id48,id49,id50,id51,id52,id53,id54,id55,id56,id57,id58,id59,id60,id61,id62,id63,id64,id65,id66,id67,id68,id69,id70,id71,id72,id73,id74,id75,id76,id77,id78,id79,id80,id81,id82,id83,id84,id85,id86,id87,id88,id89,id90,id91,id92,id93,id94,id95,id96,id97,id98,id99,id100,id101,id102,id103,id104,id105,id106,id107,id108,id109,id110,id111,id112,id113,id114,id115,id116,id117,id118,id119,id120,id121,id122,id123,id124,id125,id126,id127,id128,id129,id130,id131,id132,id133,id134,id135,id136,id137,id138,id139,id140,id141,id142,id143,id144,id145,id146,id147,id148,id149,id150,id151,id152,id153,id154,id155,id156,id157,id158,id159,id160,id161,id162,id163,id164,id165,id166,id167,id168,id169,id170,id171,id172,id173,id174,id175,id176,id177,id178,id179,id180,id181,id182,id183,id184,id185,id186,id187,id188,id189,id190,id191,id192,id193,id194,id195,id196,id197,id198,id199,id200;
-		if (condition.getIndexId()!=null){
+		if (condition.getIndexId().length>0){
 			ids = condition.getIndexId();
 			if(ids.length>200){
 				BusinessException.throwBusinessException(ErrorCode.Parameter_more_than_twentity, "选中的指标项目过多超过200个");
@@ -198,7 +198,7 @@ public class ArchitectureIndexSv extends BaseService {
 		List<ParameterCondition>params = new ArrayList<ParameterCondition>();
 		long[] ids = null;
 		long id0,id1,id2,id3,id4,id5,id6,id7,id8,id9,id10,id11,id12,id13,id14,id15,id16,id17,id18,id19,id20,id21,id22,id23,id24,id25,id26,id27,id28,id29,id30,id31,id32,id33,id34,id35,id36,id37,id38,id39,id40,id41,id42,id43,id44,id45,id46,id47,id48,id49,id50,id51,id52,id53,id54,id55,id56,id57,id58,id59,id60,id61,id62,id63,id64,id65,id66,id67,id68,id69,id70,id71,id72,id73,id74,id75,id76,id77,id78,id79,id80,id81,id82,id83,id84,id85,id86,id87,id88,id89,id90,id91,id92,id93,id94,id95,id96,id97,id98,id99,id100,id101,id102,id103,id104,id105,id106,id107,id108,id109,id110,id111,id112,id113,id114,id115,id116,id117,id118,id119,id120,id121,id122,id123,id124,id125,id126,id127,id128,id129,id130,id131,id132,id133,id134,id135,id136,id137,id138,id139,id140,id141,id142,id143,id144,id145,id146,id147,id148,id149,id150,id151,id152,id153,id154,id155,id156,id157,id158,id159,id160,id161,id162,id163,id164,id165,id166,id167,id168,id169,id170,id171,id172,id173,id174,id175,id176,id177,id178,id179,id180,id181,id182,id183,id184,id185,id186,id187,id188,id189,id190,id191,id192,id193,id194,id195,id196,id197,id198,id199,id200;
-		if (condition.getIndexId()!=null){
+		if (condition.getIndexId().length>0){
 			ids = condition.getIndexId();
 			if(ids.length>200){
 				BusinessException.throwBusinessException(ErrorCode.Parameter_more_than_twentity, "选中的指标项目过多超过200个");
@@ -297,7 +297,7 @@ public class ArchitectureIndexSv extends BaseService {
 		List<ParameterCondition>params = new ArrayList<ParameterCondition>();
 		long[] ids = null;
 		long id0,id1,id2,id3,id4,id5,id6,id7,id8,id9,id10,id11,id12,id13,id14,id15,id16,id17,id18,id19,id20,id21,id22,id23,id24,id25,id26,id27,id28,id29,id30,id31,id32,id33,id34,id35,id36,id37,id38,id39,id40,id41,id42,id43,id44,id45,id46,id47,id48,id49,id50,id51,id52,id53,id54,id55,id56,id57,id58,id59,id60,id61,id62,id63,id64,id65,id66,id67,id68,id69,id70,id71,id72,id73,id74,id75,id76,id77,id78,id79,id80,id81,id82,id83,id84,id85,id86,id87,id88,id89,id90,id91,id92,id93,id94,id95,id96,id97,id98,id99,id100,id101,id102,id103,id104,id105,id106,id107,id108,id109,id110,id111,id112,id113,id114,id115,id116,id117,id118,id119,id120,id121,id122,id123,id124,id125,id126,id127,id128,id129,id130,id131,id132,id133,id134,id135,id136,id137,id138,id139,id140,id141,id142,id143,id144,id145,id146,id147,id148,id149,id150,id151,id152,id153,id154,id155,id156,id157,id158,id159,id160,id161,id162,id163,id164,id165,id166,id167,id168,id169,id170,id171,id172,id173,id174,id175,id176,id177,id178,id179,id180,id181,id182,id183,id184,id185,id186,id187,id188,id189,id190,id191,id192,id193,id194,id195,id196,id197,id198,id199,id200;
-		if (condition.getIndexId()!=null){
+		if (condition.getIndexId().length>0){
 			ids = condition.getIndexId();
 			if(ids.length>200){
 				BusinessException.throwBusinessException(ErrorCode.Parameter_more_than_twentity, "选中的指标项目过多超过200个");
@@ -332,7 +332,7 @@ public class ArchitectureIndexSv extends BaseService {
 		List<ParameterCondition>params = new ArrayList<ParameterCondition>();
 		long[] ids = null;
 		long id0,id1,id2,id3,id4,id5,id6,id7,id8,id9,id10,id11,id12,id13,id14,id15,id16,id17,id18,id19,id20,id21,id22,id23,id24,id25,id26,id27,id28,id29,id30,id31,id32,id33,id34,id35,id36,id37,id38,id39,id40,id41,id42,id43,id44,id45,id46,id47,id48,id49,id50,id51,id52,id53,id54,id55,id56,id57,id58,id59,id60,id61,id62,id63,id64,id65,id66,id67,id68,id69,id70,id71,id72,id73,id74,id75,id76,id77,id78,id79,id80,id81,id82,id83,id84,id85,id86,id87,id88,id89,id90,id91,id92,id93,id94,id95,id96,id97,id98,id99,id100,id101,id102,id103,id104,id105,id106,id107,id108,id109,id110,id111,id112,id113,id114,id115,id116,id117,id118,id119,id120,id121,id122,id123,id124,id125,id126,id127,id128,id129,id130,id131,id132,id133,id134,id135,id136,id137,id138,id139,id140,id141,id142,id143,id144,id145,id146,id147,id148,id149,id150,id151,id152,id153,id154,id155,id156,id157,id158,id159,id160,id161,id162,id163,id164,id165,id166,id167,id168,id169,id170,id171,id172,id173,id174,id175,id176,id177,id178,id179,id180,id181,id182,id183,id184,id185,id186,id187,id188,id189,id190,id191,id192,id193,id194,id195,id196,id197,id198,id199,id200;
-		if (condition.getIndexId()!=null){
+		if (condition.getIndexId().length>0){
 			ids = condition.getIndexId();
 			if(ids.length>200){
 				BusinessException.throwBusinessException(ErrorCode.Parameter_more_than_twentity, "选中的指标项目过多超过200个");
@@ -391,7 +391,7 @@ public class ArchitectureIndexSv extends BaseService {
 		List<ParameterCondition>params = new ArrayList<ParameterCondition>();
 		long[] ids = null;
 		long id0,id1,id2,id3,id4,id5,id6,id7,id8,id9,id10,id11,id12,id13,id14,id15,id16,id17,id18,id19,id20,id21,id22,id23,id24,id25,id26,id27,id28,id29,id30,id31,id32,id33,id34,id35,id36,id37,id38,id39,id40,id41,id42,id43,id44,id45,id46,id47,id48,id49,id50,id51,id52,id53,id54,id55,id56,id57,id58,id59,id60,id61,id62,id63,id64,id65,id66,id67,id68,id69,id70,id71,id72,id73,id74,id75,id76,id77,id78,id79,id80,id81,id82,id83,id84,id85,id86,id87,id88,id89,id90,id91,id92,id93,id94,id95,id96,id97,id98,id99,id100,id101,id102,id103,id104,id105,id106,id107,id108,id109,id110,id111,id112,id113,id114,id115,id116,id117,id118,id119,id120,id121,id122,id123,id124,id125,id126,id127,id128,id129,id130,id131,id132,id133,id134,id135,id136,id137,id138,id139,id140,id141,id142,id143,id144,id145,id146,id147,id148,id149,id150,id151,id152,id153,id154,id155,id156,id157,id158,id159,id160,id161,id162,id163,id164,id165,id166,id167,id168,id169,id170,id171,id172,id173,id174,id175,id176,id177,id178,id179,id180,id181,id182,id183,id184,id185,id186,id187,id188,id189,id190,id191,id192,id193,id194,id195,id196,id197,id198,id199,id200;
-		if (condition.getIndexId()!=null){
+		if (condition.getIndexId().length>0){
 			ids = condition.getIndexId();
 			if(ids.length>200){
 				BusinessException.throwBusinessException(ErrorCode.Parameter_more_than_twentity, "选中的指标项目过多超过200个");
@@ -450,7 +450,7 @@ public class ArchitectureIndexSv extends BaseService {
 		List<ParameterCondition>params = new ArrayList<ParameterCondition>();
 		long[] ids = null;
 		long id0,id1,id2,id3,id4,id5,id6,id7,id8,id9,id10,id11,id12,id13,id14,id15,id16,id17,id18,id19,id20,id21,id22,id23,id24,id25,id26,id27,id28,id29,id30,id31,id32,id33,id34,id35,id36,id37,id38,id39,id40,id41,id42,id43,id44,id45,id46,id47,id48,id49,id50,id51,id52,id53,id54,id55,id56,id57,id58,id59,id60,id61,id62,id63,id64,id65,id66,id67,id68,id69,id70,id71,id72,id73,id74,id75,id76,id77,id78,id79,id80,id81,id82,id83,id84,id85,id86,id87,id88,id89,id90,id91,id92,id93,id94,id95,id96,id97,id98,id99,id100,id101,id102,id103,id104,id105,id106,id107,id108,id109,id110,id111,id112,id113,id114,id115,id116,id117,id118,id119,id120,id121,id122,id123,id124,id125,id126,id127,id128,id129,id130,id131,id132,id133,id134,id135,id136,id137,id138,id139,id140,id141,id142,id143,id144,id145,id146,id147,id148,id149,id150,id151,id152,id153,id154,id155,id156,id157,id158,id159,id160,id161,id162,id163,id164,id165,id166,id167,id168,id169,id170,id171,id172,id173,id174,id175,id176,id177,id178,id179,id180,id181,id182,id183,id184,id185,id186,id187,id188,id189,id190,id191,id192,id193,id194,id195,id196,id197,id198,id199,id200;
-		if (condition.getIndexId()!=null){
+		if (condition.getIndexId().length>0){
 			ids = condition.getIndexId();
 			if(ids.length>200){
 				BusinessException.throwBusinessException(ErrorCode.Parameter_more_than_twentity, "选中的指标项目过多超过200个");
@@ -509,7 +509,7 @@ public class ArchitectureIndexSv extends BaseService {
 		List<ParameterCondition>params = new ArrayList<ParameterCondition>();
 		long[] ids = null;
 		long id0,id1,id2,id3,id4,id5,id6,id7,id8,id9,id10,id11,id12,id13,id14,id15,id16,id17,id18,id19,id20,id21,id22,id23,id24,id25,id26,id27,id28,id29,id30,id31,id32,id33,id34,id35,id36,id37,id38,id39,id40,id41,id42,id43,id44,id45,id46,id47,id48,id49,id50,id51,id52,id53,id54,id55,id56,id57,id58,id59,id60,id61,id62,id63,id64,id65,id66,id67,id68,id69,id70,id71,id72,id73,id74,id75,id76,id77,id78,id79,id80,id81,id82,id83,id84,id85,id86,id87,id88,id89,id90,id91,id92,id93,id94,id95,id96,id97,id98,id99,id100,id101,id102,id103,id104,id105,id106,id107,id108,id109,id110,id111,id112,id113,id114,id115,id116,id117,id118,id119,id120,id121,id122,id123,id124,id125,id126,id127,id128,id129,id130,id131,id132,id133,id134,id135,id136,id137,id138,id139,id140,id141,id142,id143,id144,id145,id146,id147,id148,id149,id150,id151,id152,id153,id154,id155,id156,id157,id158,id159,id160,id161,id162,id163,id164,id165,id166,id167,id168,id169,id170,id171,id172,id173,id174,id175,id176,id177,id178,id179,id180,id181,id182,id183,id184,id185,id186,id187,id188,id189,id190,id191,id192,id193,id194,id195,id196,id197,id198,id199,id200;
-		if (condition.getIndexId()!=null){
+		if (condition.getIndexId().length>0){
 			ids = condition.getIndexId();
 			if(ids.length>200){
 				BusinessException.throwBusinessException(ErrorCode.Parameter_more_than_twentity, "选中的指标项目过多超过200个");
@@ -568,7 +568,7 @@ public class ArchitectureIndexSv extends BaseService {
 		List<ParameterCondition>params = new ArrayList<ParameterCondition>();
 		long[] ids = null;
 		long id0,id1,id2,id3,id4,id5,id6,id7,id8,id9,id10,id11,id12,id13,id14,id15,id16,id17,id18,id19,id20,id21,id22,id23,id24,id25,id26,id27,id28,id29,id30,id31,id32,id33,id34,id35,id36,id37,id38,id39,id40,id41,id42,id43,id44,id45,id46,id47,id48,id49,id50,id51,id52,id53,id54,id55,id56,id57,id58,id59,id60,id61,id62,id63,id64,id65,id66,id67,id68,id69,id70,id71,id72,id73,id74,id75,id76,id77,id78,id79,id80,id81,id82,id83,id84,id85,id86,id87,id88,id89,id90,id91,id92,id93,id94,id95,id96,id97,id98,id99,id100,id101,id102,id103,id104,id105,id106,id107,id108,id109,id110,id111,id112,id113,id114,id115,id116,id117,id118,id119,id120,id121,id122,id123,id124,id125,id126,id127,id128,id129,id130,id131,id132,id133,id134,id135,id136,id137,id138,id139,id140,id141,id142,id143,id144,id145,id146,id147,id148,id149,id150,id151,id152,id153,id154,id155,id156,id157,id158,id159,id160,id161,id162,id163,id164,id165,id166,id167,id168,id169,id170,id171,id172,id173,id174,id175,id176,id177,id178,id179,id180,id181,id182,id183,id184,id185,id186,id187,id188,id189,id190,id191,id192,id193,id194,id195,id196,id197,id198,id199,id200;
-		if (condition.getIndexId()!=null){
+		if (condition.getIndexId().length>0){
 			ids = condition.getIndexId();
 			if(ids.length>200){
 				BusinessException.throwBusinessException(ErrorCode.Parameter_more_than_twentity, "选中的指标项目过多超过200个");
@@ -611,7 +611,7 @@ public class ArchitectureIndexSv extends BaseService {
 		List<ParameterCondition>params = new ArrayList<ParameterCondition>();
 		long[] ids = null;
 		long id0,id1,id2,id3,id4,id5,id6,id7,id8,id9,id10,id11,id12,id13,id14,id15,id16,id17,id18,id19,id20,id21,id22,id23,id24,id25,id26,id27,id28,id29,id30,id31,id32,id33,id34,id35,id36,id37,id38,id39,id40,id41,id42,id43,id44,id45,id46,id47,id48,id49,id50,id51,id52,id53,id54,id55,id56,id57,id58,id59,id60,id61,id62,id63,id64,id65,id66,id67,id68,id69,id70,id71,id72,id73,id74,id75,id76,id77,id78,id79,id80,id81,id82,id83,id84,id85,id86,id87,id88,id89,id90,id91,id92,id93,id94,id95,id96,id97,id98,id99,id100,id101,id102,id103,id104,id105,id106,id107,id108,id109,id110,id111,id112,id113,id114,id115,id116,id117,id118,id119,id120,id121,id122,id123,id124,id125,id126,id127,id128,id129,id130,id131,id132,id133,id134,id135,id136,id137,id138,id139,id140,id141,id142,id143,id144,id145,id146,id147,id148,id149,id150,id151,id152,id153,id154,id155,id156,id157,id158,id159,id160,id161,id162,id163,id164,id165,id166,id167,id168,id169,id170,id171,id172,id173,id174,id175,id176,id177,id178,id179,id180,id181,id182,id183,id184,id185,id186,id187,id188,id189,id190,id191,id192,id193,id194,id195,id196,id197,id198,id199,id200;
-		if (condition.getIndexId()!=null){
+		if (condition.getIndexId().length>0){
 			ids = condition.getIndexId();
 			if(ids.length>200){
 				BusinessException.throwBusinessException(ErrorCode.Parameter_more_than_twentity, "选中的指标项目过多超过200个");
@@ -684,7 +684,7 @@ public class ArchitectureIndexSv extends BaseService {
 		List<ParameterCondition>params = new ArrayList<ParameterCondition>();
 		long[] ids = null;
 		long id0,id1,id2,id3,id4,id5,id6,id7,id8,id9,id10,id11,id12,id13,id14,id15,id16,id17,id18,id19,id20,id21,id22,id23,id24,id25,id26,id27,id28,id29,id30,id31,id32,id33,id34,id35,id36,id37,id38,id39,id40,id41,id42,id43,id44,id45,id46,id47,id48,id49,id50,id51,id52,id53,id54,id55,id56,id57,id58,id59,id60,id61,id62,id63,id64,id65,id66,id67,id68,id69,id70,id71,id72,id73,id74,id75,id76,id77,id78,id79,id80,id81,id82,id83,id84,id85,id86,id87,id88,id89,id90,id91,id92,id93,id94,id95,id96,id97,id98,id99,id100,id101,id102,id103,id104,id105,id106,id107,id108,id109,id110,id111,id112,id113,id114,id115,id116,id117,id118,id119,id120,id121,id122,id123,id124,id125,id126,id127,id128,id129,id130,id131,id132,id133,id134,id135,id136,id137,id138,id139,id140,id141,id142,id143,id144,id145,id146,id147,id148,id149,id150,id151,id152,id153,id154,id155,id156,id157,id158,id159,id160,id161,id162,id163,id164,id165,id166,id167,id168,id169,id170,id171,id172,id173,id174,id175,id176,id177,id178,id179,id180,id181,id182,id183,id184,id185,id186,id187,id188,id189,id190,id191,id192,id193,id194,id195,id196,id197,id198,id199,id200;
-		if (condition.getIndexId()!=null){
+		if (condition.getIndexId().length>0){
 			ids = condition.getIndexId();
 			if(ids.length>200){
 				BusinessException.throwBusinessException(ErrorCode.Parameter_more_than_twentity, "选中的指标项目过多超过200个");
@@ -749,7 +749,7 @@ public class ArchitectureIndexSv extends BaseService {
 		List<ParameterCondition>params = new ArrayList<ParameterCondition>();
 		long[] ids = null;
 		long id0,id1,id2,id3,id4,id5,id6,id7,id8,id9,id10,id11,id12,id13,id14,id15,id16,id17,id18,id19,id20,id21,id22,id23,id24,id25,id26,id27,id28,id29,id30,id31,id32,id33,id34,id35,id36,id37,id38,id39,id40,id41,id42,id43,id44,id45,id46,id47,id48,id49,id50,id51,id52,id53,id54,id55,id56,id57,id58,id59,id60,id61,id62,id63,id64,id65,id66,id67,id68,id69,id70,id71,id72,id73,id74,id75,id76,id77,id78,id79,id80,id81,id82,id83,id84,id85,id86,id87,id88,id89,id90,id91,id92,id93,id94,id95,id96,id97,id98,id99,id100,id101,id102,id103,id104,id105,id106,id107,id108,id109,id110,id111,id112,id113,id114,id115,id116,id117,id118,id119,id120,id121,id122,id123,id124,id125,id126,id127,id128,id129,id130,id131,id132,id133,id134,id135,id136,id137,id138,id139,id140,id141,id142,id143,id144,id145,id146,id147,id148,id149,id150,id151,id152,id153,id154,id155,id156,id157,id158,id159,id160,id161,id162,id163,id164,id165,id166,id167,id168,id169,id170,id171,id172,id173,id174,id175,id176,id177,id178,id179,id180,id181,id182,id183,id184,id185,id186,id187,id188,id189,id190,id191,id192,id193,id194,id195,id196,id197,id198,id199,id200;
-		if (condition.getIndexId()!=null){
+		if (condition.getIndexId().length>0){
 			ids = condition.getIndexId();
 			if(ids.length>200){
 				BusinessException.throwBusinessException(ErrorCode.Parameter_more_than_twentity, "选中的指标项目过多超过200个");
@@ -823,7 +823,7 @@ public class ArchitectureIndexSv extends BaseService {
 		List<ParameterCondition>params = new ArrayList<ParameterCondition>();
 		long[] ids = null;
 		long id0,id1,id2,id3,id4,id5,id6,id7,id8,id9,id10,id11,id12,id13,id14,id15,id16,id17,id18,id19,id20,id21,id22,id23,id24,id25,id26,id27,id28,id29,id30,id31,id32,id33,id34,id35,id36,id37,id38,id39,id40,id41,id42,id43,id44,id45,id46,id47,id48,id49,id50,id51,id52,id53,id54,id55,id56,id57,id58,id59,id60,id61,id62,id63,id64,id65,id66,id67,id68,id69,id70,id71,id72,id73,id74,id75,id76,id77,id78,id79,id80,id81,id82,id83,id84,id85,id86,id87,id88,id89,id90,id91,id92,id93,id94,id95,id96,id97,id98,id99,id100,id101,id102,id103,id104,id105,id106,id107,id108,id109,id110,id111,id112,id113,id114,id115,id116,id117,id118,id119,id120,id121,id122,id123,id124,id125,id126,id127,id128,id129,id130,id131,id132,id133,id134,id135,id136,id137,id138,id139,id140,id141,id142,id143,id144,id145,id146,id147,id148,id149,id150,id151,id152,id153,id154,id155,id156,id157,id158,id159,id160,id161,id162,id163,id164,id165,id166,id167,id168,id169,id170,id171,id172,id173,id174,id175,id176,id177,id178,id179,id180,id181,id182,id183,id184,id185,id186,id187,id188,id189,id190,id191,id192,id193,id194,id195,id196,id197,id198,id199,id200;
-		if (condition.getIndexId()!=null){
+		if (condition.getIndexId().length>0){
 			ids = condition.getIndexId();
 			if(ids.length>200){
 				BusinessException.throwBusinessException(ErrorCode.Parameter_more_than_twentity, "选中的指标项目过多超过200个");
@@ -880,7 +880,7 @@ public class ArchitectureIndexSv extends BaseService {
 		List<ParameterCondition>params = new ArrayList<ParameterCondition>();
 		long[] ids = null;
 		long id0,id1,id2,id3,id4,id5,id6,id7,id8,id9,id10,id11,id12,id13,id14,id15,id16,id17,id18,id19,id20,id21,id22,id23,id24,id25,id26,id27,id28,id29,id30,id31,id32,id33,id34,id35,id36,id37,id38,id39,id40,id41,id42,id43,id44,id45,id46,id47,id48,id49,id50,id51,id52,id53,id54,id55,id56,id57,id58,id59,id60,id61,id62,id63,id64,id65,id66,id67,id68,id69,id70,id71,id72,id73,id74,id75,id76,id77,id78,id79,id80,id81,id82,id83,id84,id85,id86,id87,id88,id89,id90,id91,id92,id93,id94,id95,id96,id97,id98,id99,id100,id101,id102,id103,id104,id105,id106,id107,id108,id109,id110,id111,id112,id113,id114,id115,id116,id117,id118,id119,id120,id121,id122,id123,id124,id125,id126,id127,id128,id129,id130,id131,id132,id133,id134,id135,id136,id137,id138,id139,id140,id141,id142,id143,id144,id145,id146,id147,id148,id149,id150,id151,id152,id153,id154,id155,id156,id157,id158,id159,id160,id161,id162,id163,id164,id165,id166,id167,id168,id169,id170,id171,id172,id173,id174,id175,id176,id177,id178,id179,id180,id181,id182,id183,id184,id185,id186,id187,id188,id189,id190,id191,id192,id193,id194,id195,id196,id197,id198,id199,id200;
-		if (condition.getIndexId()!=null){
+		if (condition.getIndexId().length>0){
 			ids = condition.getIndexId();
 			if(ids.length>200){
 				BusinessException.throwBusinessException(ErrorCode.Parameter_more_than_twentity, "选中的指标项目过多超过200个");

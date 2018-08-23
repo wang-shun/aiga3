1656d0cd6c697fc250722663df23248d7f6b55fb
dupeng <dupeng5@asiainfo.com>
2018-08-23 11:03:19 +0800

    文件下载

---
+++ aiga3/src/main/java/com/ai/aiga/view/controller/inspectradar/dto/InspectRadarFileInfo.java
@@ -0,0 +1,10 @@
+package com.ai.aiga.view.controller.inspectradar.dto;
+
+import lombok.Data;
+
+@Data
+public class InspectRadarFileInfo {
+	private String sysId;
+	private Long processFile;
+	private Long deployFile;
+}
